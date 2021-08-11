d0a9adcd5c6497e6ac5fabace693cbc203576600
Justin Beere <beere.justin@gmail.com>
2021-08-10 19:42:28 -0700

    Implemented MVP - TODO implement feature to handle rounds parameter

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,11 +18,28 @@
 *
 */
 
-var rockPaperScissors = function (rounds) {
+var rockPaperScissors = function () {
   var moveSet = ['R', 'P', 'S'];
+  var moveCombos = [];
+  var rounds = 3;
+
   // could we iterate across the array and push the move into the current rounds array
-  // only if that move doesnt exist at that round index?
-  return [];
+  var possibleMoves = function(currentMoves) {
+    // check if this is the first move or if we already have a string of current moves
+    currentMoves = currentMoves || '';
+    // check the length of current moves against rounds and add it to the move combos array
+    if ( currentMoves.length === rounds ) {
+      moveCombos.push(currentMoves);
+      return;
+    }
+    moveSet.forEach(function(move) {
+      possibleMoves(currentMoves + move);
+    });
+  };
+  // start the recursive function call to assemble the list of all possible moves
+  possibleMoves();
+
+  return moveCombos;
 };
 
 module.exports = rockPaperScissors;
