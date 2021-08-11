55851ef51c5d59a1e6e90d40c43e1377b8f6394b
Alex Romero <alexromeroreyes09@gmail.com>
2021-08-10 19:57:32 -0700

    Implemented a partially working solution, will continue to work on the problem.

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,31 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function (number) {
+  // Create a result array
+  var combos = [];
+  // Create a combo string
+  var comboString = '';
+  // Establish player options (array)
+  var playerOptions = ['R', 'P', 'S'];
+  // Iterate through player options array
+  for (var i = 0; i < playerOptions.length; i++) {
+    // Push value at i into combo string
+    comboString += playerOptions[i];
+    // Iterate again
+    for (var j = 0; j < playerOptions.length; j++) {
+      // Push into combo string again
+      comboString += playerOptions[j];
+      // Iterate again
+      for (var k = 0; k < playerOptions.length; k++) {
+        // Push into combo string again
+        comboString += playerOptions[k];
+        // Push combo string into result array
+        combos.push(comboString);
+      }
+    }
+  }
+  // Return result array
+  return combos;
 };
 
