c10e462c8e1d88f998426b9d26b21550e5071f6d
Bishal Gautam <bgautam.se@gmail.com>
2021-08-10 20:41:45 -0700

    Completed Toy Problem 1

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,35 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function (rounds) {
+  var choices = ['R','P','S'];
+  var combos = [];
+  var rounds = rounds || 3;
+
+  var genCombos = function(game) {
+    var game = game || '';
+
+    if (game.length === rounds) {
+      combos.push(game);
+    } else {
+      for (var i = 0; i < choices.length; i++) {
+        currentGame = game + choices[i];
+        genCombos(currentGame);
+      }
+    }
+
+  }
+  genCombos();
+  //innner function is called recursively
+    //check if combo str length === 3, if so, push combo to results and return back to previous recursive call which will continue the loop
+    //loops over choices
+      //for each one, concat it to game hands
+        //recursively call inner function and pass in currentGame hands -- this is tricky, bc cannot change the value of game before passing it in
+
+  //invoke inner function
+
+  return combos;
 };
+
+
 
