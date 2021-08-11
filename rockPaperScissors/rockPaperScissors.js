4e326e3cfe08cd78d65142819878a0b310ad379a
Andy Fang <afang1094@gmail.com>
2021-08-10 22:04:45 -0700

    deleted psuedocode

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -49,31 +49,19 @@
   // rockPaperScissors();
 
   var rockPaperScissors = function (rounds, item) {
-    // declare a result array
     var storage = [];
-    // declare a var to store the three rounds
     var item = item || '';
-    // declare game variables
     var gameVariables = ['R', 'P', 'S'];
 
-    // check if rounds have a length of three
     if (item.length === rounds) {
-      // push it to storage array
       return item;
-  //     return;
     }
 
-    // iterate over game variables
     for (var i = 0; i < gameVariables.length; i++) {
-      // recursively call rockPaperScissors on each variable
       var currentItem = gameVariables[i];
-  //     rounds = rounds.concat(rockPaperScissors(currentItem));
          storage = storage.concat(rockPaperScissors(rounds, item + currentItem));
-  //     rounds += currentItem;
     }
 
-    // retun storage
     return storage;
   };
-
   rockPaperScissors(3);
