7f8c5c88e3999363471fdbeaef99720f30eae0ed
Daniel Ho <dho1994@gmail.com>
2021-08-10 20:00:12 -0700

    Implemented rock paper scissors (easy mode)

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,129 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function () {
   // TODO: your solution here
+  var moves = ['R', 'P', 'S'];
+  var output = [];
+
+  for (var a = 0; a < moves.length; a++) {
+    for (var b = 0; b < moves.length; b++) {
+      for (var c = 0; c < moves.length; c++) {
+        output.push(moves[a] + moves[b] + moves[c]);
+      }
+    }
+  }
+
+  return output;
 };
 
+/*
+RPS
+RPS
+RPS
+*/
+
+
+// var rockPaperScissors = function (rounds) {
+//   // TODO: your solution here
+//   var moves = ['R', 'P', 'S'];
+//   var loops = 0;
+//   var output = [];
+//   var combo = '';
+
+//   var innerRockPaperScissors = function(rounds) {
+//     loops++;
+//     if (loops > rounds) {
+//       return;
+//     }
+//     moves.forEach(function(value){
+//       combo += value;
+//       if (combo.length === rounds) {
+//         output.push(combo);
+//         return;
+//       }
+//       innerRockPaperScissors(rounds);
+//     })
+//   }
+//   innerRockPaperScissors(rounds);
+//   return output;
+// };
+
+
+
+
+// var rockPaperScissors = function (rounds) {
+//   // TODO: your solution here
+//   var moves = ['R', 'P', 'S'];
+//   var loops = 0;
+//   var output = [];
+//   var combo = '';
+
+//   var innerRockPaperScissors = function(rounds) {
+//     loops++;
+//     moves.forEach(function(value){
+//       combo += value;
+//       if (combo.length === rounds) {
+//         output.push(combo);
+//         return;
+//       }
+//       if (loops <= rounds) {innerRockPaperScissors(rounds)};
+//     })
+//   }
+//   innerRockPaperScissors(rounds);
+//   return output;
+// };
+
+
+
+// var rockPaperScissors = function (rounds) {
+//   // TODO: your solution here
+//   var moves = ['R', 'P', 'S'];
+//   var loops = 0;
+//   var output = [];
+//   var combo = '';
+
+//   var innerRockPaperScissors = function(rounds) {
+//     loops++;
+//     moves.forEach(function(value){
+//       combo += value;
+//       if (combo.length === rounds) {
+//         output.push(combo);
+//         combo = combo.substring(0, combo.length - 1)
+//         return;
+//       }
+//       if (loops <= rounds) {innerRockPaperScissors(rounds)};
+//     })
+//   }
+//   innerRockPaperScissors(rounds);
+//   return output;
+// };
+
+
+
+
+
+
+
+
+// var rockPaperScissors = function (rounds) {
+//   // TODO: your solution here
+//   var moves = ['R', 'P', 'S'];
+//   var loops = 0;
+//   var output = [];
+//   var combo = '';
+
+//   var innerRockPaperScissors = function(rounds) {
+//     loops++
+//     for (var i = 0; i < moves.length; i++) {
+//       var letter = moves[i];
+//       if (loops === rounds) {
+//         return letter;
+//       }
+//       letter += innerRockPaperScissors(rounds);
+//       output.push(letter);
+//     }
+//   }
+//   innerRockPaperScissors(rounds);
+//   return output;
+// };
