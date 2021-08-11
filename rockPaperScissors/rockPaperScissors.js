796cacdf50f35c178de1b3208df4c284187e02ec
Justin <jmitchell31@g.ucla.edu>
2021-08-10 21:10:25 -0700

    First Pull Request

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,39 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function () {
+
+  //create answers array with possible answers
+  var answers = ['R','P','S'];
+  var rounds = 3;
+
+  //on first iteration, create result array with answers times number of rounds spots (3 rounds -> ['r','p','s','r','p','s','r','p','s'])
+
+
+  var result = [];
+
+  for (var i = 0; i < 3; i++) {
+    for(var j = 0; j < answers.length; j++) {
+      result.push(answers[j]);
+    }
+  }
+
+  var getSequences = function(rounds) {
+    var count = 0;
+    for(var i = 0; i < result.length; i++) {
+        var counter = 0;
+        if (i !== 0 && i % 3 === 0) {
+           count++;
+        }
+        while (counter < 2) {
+           result[i] = result[i].concat(answers[count]);
+           counter++;
+       }
+    }
+  }
+  var results = getSequences(rounds);
+  return result;
 };
+
+
 
