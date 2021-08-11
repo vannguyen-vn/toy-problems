50fe4da1255b2c9c43464200ccd2bc650cbf619b
Andrew Cho <andrewgunncho@gmail.com>
2021-08-10 20:18:32 -0700

    Work in progress

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,35 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function (combo, rounds) {
+  var result = [];
+  var combo = combo || '';
+  var rounds = rounds || 0;
+  // base
+  // if combo length is 3
+  if (combo.length === 3) {
+    // add into the result
+    result.push(combo);
+    return result;
+  }
+
+  rounds++;
+
+
+  // recursion
+  // ['R', 'P', 'S']
+  var comboMoves = ['R', 'P', 'S'];
+  // add a character into our combo
+  for (var i = 0; i < comboMoves.length; i++) {
+    combo += comboMoves[i];
+    result = result.concat(rockPaperScissors(combo, rounds));
+    rounds++;
+    if (rounds >= 3) {
+      return result;
+    }
+  }
+    // run our function on that combo
+
+  return result;
 };
 
