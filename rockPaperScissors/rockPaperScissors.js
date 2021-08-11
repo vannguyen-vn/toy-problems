d843189a8c13db1ac6c0a68b7ce0d049274679f5
Caleb Kim <kim.caleb10@gmail.com>
2021-08-10 19:44:41 -0700

    Implemented Rock Paper Scissors

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,29 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function () {
+  // create options for rock-paper-scissors
+  var options = ['R', 'P', 'S'];
+  // create a result array for the outcomes of rock-paper-scissors
+  var combinations = [];
+  // create a combo string
+  var combo = '';
+  // create an inner recursive function taking in combo
+  var recursive = function(combo) {
+    if (combo.length === 3) {
+    // push the string into result array declared in outer scope
+      combinations.push(combo);
+      return;
+    }
+    // iterate over the options array
+    for (var i = 0; i < options.length; i++) {
+      // use recursive function
+        recursive(combo + options[i]);
+    }
+  }
+  recursive(combo);
+  // return result array
+  return combinations;
 };
 
+rockPaperScissors();
