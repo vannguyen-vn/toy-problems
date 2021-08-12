b0fdb0b13d3798825f3239e550c2131cb6649483
Gabrielle Davidson <gdvdsn@gmail.com>
2021-08-11 18:05:25 -1000

    Toy problem complete

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -11,36 +11,31 @@
 */
 
 var evenOccurrence = function(arr) {
-  // create an empty array
   var presentNums = [];
-  // create an empty object
   var duplicateNums = {};
 
-  // iterate over the input arr
   for (var num of arr){
-    // if the array contains the current number
-    if (arr.includes(num)) {
-      // if the object contains the number as a key
+    if (presentNums.includes(num)) {
+      presentNums.push(num);
       if (duplicateNums.hasOwnProperty(num)) {
-        // increase the value by one
         duplicateNums[num] = duplicateNums[num] + 1;
-      // else
       } else {
-        // add the number as a key with value of 2
         duplicateNums[num] = 2;
       }
     } else {
-    // else
-      // add it to the empty array
       presentNums.push(num);
     }
   }
 
-  // create another empty object
+  var numsWithEvenOccurrences = {};
 
-  //iterate over the object with number and occurence keys and values
-    // if occurence is and an even number
-      // add that key/value pair to the empty object
+  for (var key in duplicateNums) {
+    if (duplicateNums[key] % 2 === 0) {
+      numsWithEvenOccurrences[key] = arr.indexOf(Number(key));
+    }
+  }
 
-  // find the indexOf each key in the original array and return the key with the smallest index
+  var smallestIndex = Math.min(...Object.values(numsWithEvenOccurrences))
+
+  return arr[smallestIndex];
 };
