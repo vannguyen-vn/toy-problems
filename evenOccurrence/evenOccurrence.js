c1d9cf0049f85012505320bdc514b720bc5a3e54
Isabell Lee <izzy.isabell@gmail.com>
2021-08-11 19:29:46 -0700

    Completed evenOccurence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -11,5 +11,11 @@
 */
 
 var evenOccurrence = function(arr) {
-  // Your code here.
+  for (var i = 0; i < arr.length; i++) {
+    var checkArr = arr.slice(i + 1);
+    if (checkArr.includes(arr[i])) {
+      return arr[i];
+    }
+  }
+  return null;
 };
