e4f71e157ec69462cc8b42d1828535e66f9cf7ee
Salar Malik <salarxmalik@gmail.com>
2021-08-11 20:13:43 -0700

    Completed Toy Problem 2

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -12,4 +12,22 @@
 
 var evenOccurrence = function(arr) {
   // Your code here.
+  var evensContainer = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evensContainer.push(arr[i]);
+    }
+  }
+  if (evensContainer.length === 0) {
+    return null;
+  }
+  var multiplesContainer = [];
+  for (var j = 0; j < evensContainer.length; j++) {
+    var currentNum = evensContainer[j];
+    var nextNum = evensContainer[j + 1];
+    if (currentNum = nextNum) {
+      multiplesContainer.push(currentNum);
+    }
+  }
+  return multiplesContainer[0];
 };
