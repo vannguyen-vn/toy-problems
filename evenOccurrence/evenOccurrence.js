4362e1ee9632521c93f2e1eaf64abb94cf4baeb4
Nan Jiang <dora.nan.j@gmail.com>
2021-08-11 19:39:56 -0700

    evenOccurrence complete

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -11,5 +11,40 @@
 */
 
 var evenOccurrence = function(arr) {
-  // Your code here.
+
+  // instantiation to store value, index and occurence
+  var makeNewElement = function(index) {
+    var newElement = {};
+
+    newElement.index = index;
+    newElement.occurrence = 1;
+
+    return newElement;
+  }
+
+  var occurrenceStorage = {};
+
+  // create new instance for each element in input array
+  for (var i = 0; i < arr.length; i++) {
+    if ( arr[i] in occurrenceStorage ) {
+      occurrenceStorage[arr[i]].occurrence += 1;
+    } else {
+      occurrenceStorage[arr[i]] = makeNewElement(i);
+    }
+  }
+
+  var firstIndex = null;
+
+  // iterate over output storage
+  // filter elements of even occurence
+  for (var j in occurrenceStorage) {
+    if (occurrenceStorage[j].occurrence % 2 === 0) {
+      // update first even occurrence value OR
+      // if an earlier element is found
+      if (firstIndex === null || occurrenceStorage[j].index < firstIndex) {
+        firstIndex = occurrenceStorage[j].index;
+    }
+  }
+}
+  return firstIndex === null ? null :arr[firstIndex];
 };
