410ca5502d688254d1a55f44e5cba983825a8ace
Van Nguyen <van.nguyen@texo.design>
2021-08-11 20:01:16 -0700

    Write a function that generates every sequence of throws a single

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -10,6 +10,25 @@
  * console.log(onlyEven); //  4
 */
 
-var evenOccurrence = function(arr) {
-  // Your code here.
+/*
+  define a storage
+  loop through the input
+    add each unique value to storage and count of each value
+  loop through the input
+    // if current item that it's count inside the storage is even
+      // return that item
+  if nothing return null;
+*/
+
+var evenOccurrence = function (arr) {
+  var storage = {};
+  for (var num of arr) {
+    storage[num] = storage[num] + 1 || 1;
+  }
+  for (var i = 0; i < arr.length; i++) {
+    var current = arr[i];
+    if (storage[current] % 2 === 0) {
+      return current;
+    }
+  }
 };
