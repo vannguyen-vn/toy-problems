27a85e2452dac1d0751a04d0a65911c51f83b9fd
Andy Fang <afang1094@gmail.com>
2021-08-11 19:49:38 -0700

    first attempt for evenOccurence and updated commit for RPS

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -12,4 +12,22 @@
 
 var evenOccurrence = function(arr) {
   // Your code here.
+
+  // create variable for result
+  var result = null;
+  // iterate through array
+  for (var i = 0; i < arr.length; i++) {
+    // iterate through array, ahead of first increment
+    for (var j = 1; j < arr.length - 1; j++) {
+      // declare current number
+      var current = arr[i];
+      var next = arr[j];
+      // check if second increment matches current number
+      if (current === next);
+      // add current number to result
+      result += current;
+    }
+  }
+  // return null
+  return result;
 };
