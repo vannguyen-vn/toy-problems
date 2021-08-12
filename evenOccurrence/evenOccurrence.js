695643f84c8ed5023c72752bee6c4ebd0c1ac807
Andrew Cho <andrewgunncho@gmail.com>
2021-08-11 19:36:57 -0700

    Finished Assignment

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -11,5 +11,24 @@
 */
 
 var evenOccurrence = function(arr) {
-  // Your code here.
+  var obj = {};
+  var container = [];
+
+  arr.forEach(function(item) {
+      if (obj[item] === undefined) {
+        obj[item] = true;
+      } else {
+        container.push(item);
+      }
+  });
+
+  for (var i = 0; i < arr.length; i ++){
+    var currentValue = arr[i];
+    for (var j = 0; j < container.length; j++) {
+      if (container[j] === currentValue) {
+        return currentValue;
+      }
+    }
+  }
+  return null;
 };
