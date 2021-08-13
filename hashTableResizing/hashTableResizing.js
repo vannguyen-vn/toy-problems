ddd3f8fd8fb10e504fc61e58f8c1876a6961fb19
Bishal Gautam <bgautam.se@gmail.com>
2021-08-12 21:07:10 -0700

    Completed HashTable with resizing

--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -22,23 +22,102 @@ var getIndexBelowMaxForKey = function(str, max) {
 
 var makeHashTable = function() {
   var result = {};
-  var storage = [];
-  var storageLimit = 4;
-  var size = 0;
-  
-  result.insert = function(/*...*/ 
-) {
-    // TODO: implement `insert`
+  result.storage = [];
+  result._storageLimit = 4;
+  result._size = 0;
+
+  result.insert = function(key, value) {
+    //generate new index
+    var index = getIndexBelowMaxForKey(key, result._storageLimit);
+
+    //create the bucket and tuple
+    var bucket = [];
+    var tuple = [key, value];
+    //push tuple into bucket
+
+    //if the value at the index is undefined, then put the bucket there
+    //if the index already exists, then push the tuple into the bucket
+    if (!result.storage[index]) {
+      bucket.push(tuple);
+      result._size++;
+      result.storage[index] = bucket;
+    } else if (result.storage[index]) {
+      if (result.storage[index][key] !== undefined) {
+        //if the key already exists, replace the value at the key with the new one, no need to do size++
+        result.storage[index][key] = value;
+      } else {
+        //else push the tuple into the bucket at that storage index, size++
+        result.storage[index].push(tuple);
+        result._size++;
+
+      }
+    }
+
+    if ((result._size/result._storageLimit) > 0.75) {
+      result.resize(result.storage, result._storageLimit*2);
+    }
+    // if the length of the storage array is 75% full of tuples (ie. size var), then expand the storageLimit by a factor of two
+    //call resize function
+
   };
 
-  result.retrieve = function(/*...*/ 
-) {
-    // TODO: implement `retrieve`
+  result.retrieve = function(key) {
+    //give a key, return a value
+    var index = getIndexBelowMaxForKey(key, result._storageLimit);
+    var bucket = result.storage[index];
+    if (bucket) {
+      for (var i = 0; i < result.storage[index].length; i++) {
+        if (bucket[i][0] === key) {
+          return bucket[i][1];
+        }
+      }
+    } else {
+      return null;
+    }
+
+    //check if that key is present
+    //return a value if that key is present
   };
 
-  result.remove = function(/*...*/ 
-) {
-    // TODO: implement `remove`
+  result.remove = function(key) {
+    //give a key, check if that key is present
+    var index = getIndexBelowMaxForKey(key, result._storageLimit);
+    var bucket = result.storage[index];
+    if (bucket) {
+      for (var i = 0; i < result.storage[index].length; i++) {
+        if (bucket[i][0] === key) {
+          bucket.splice(i, 1);
+          result._size--;
+        }
+      }
+    }
+    //if size/storageLength is now below 25%, resize
+    if ( (result._size/result._storageLimit) < 0.25) {
+      result.resize(result.storage, result._storageLimit/2);
+    }
+
+    return null;
+
+  };
+
+  result.resize = function(storage, newLimit) {
+    //double the size if tupleCount/storageLength is >75%, and redistribute values
+    //half the size if tupleCount/storageLength is <25%, and redistribute values
+    var oldStorage = result.storage;
+    result.storage = [];
+    result._storageLimit = newLimit;
+    result._size = 0;
+
+    oldStorage.forEach(function(bucket) {
+      if (bucket === undefined) { return; }
+      for (var i = 0; i < bucket.length; i++) {
+        var key = bucket[i][0];
+        var value = bucket[i][1];
+        result.insert(key, value);
+      }
+    });
+
+
   };
 
   return result;
