304405a10fa4fe89f2205f66fabffc0484d58732
lucyjunejack <gkvoid3@gmail.com>
2021-08-12 21:58:55 -0700

    assignment completed

--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -20,26 +20,106 @@ var getIndexBelowMaxForKey = function(str, max) {
   return hash % max;
 };
 
+// var resize = function(oldSize, stoRage) {
+//     var oldStorage = stoRage;
+//     console.log(oldStorage);
+//     var newResult = makeHashTable();
+//     var newStorage = [];
+//     var newStorageLimit = oldSize * 2;
+//     var newSize = 0;
+//     for (var i = 0; i < oldStorage.length; i++) {
+//       var s = oldStorage[i];
+//       if (s !== undefined) {
+//         for (var j = 0; j < s.length; j++) {
+//           var tuple = s[j];
+//           newResult.insert(tuple[0]);
+//         }
+//       } else {
+//         continue;
+//       }
+//     }
+//     return newResult;
+//   }
+
+
 var makeHashTable = function() {
   var result = {};
   var storage = [];
   var storageLimit = 4;
   var size = 0;
-  
-  result.insert = function(/*...*/ 
-) {
-    // TODO: implement `insert`
+
+
+  result.insert = function(str) {
+    var index = getIndexBelowMaxForKey(str, storageLimit);
+    var currentBucket = storage[index];
+    var tuple = [];
+    tuple.push(str);
+    //console.log(tuple);
+    if (currentBucket === undefined) {
+      var newBucket = [];
+      newBucket.push(tuple);
+      //console.log(newBucket);
+      storage[index] = newBucket;
+      //console.log(storage);
+    } else {
+      storage[index].push(tuple);
+    }
+    size += 1;
+    console.log(storage);
+    //console.log(size);
+    //console.log(storageLimit * 0.75);
+    // if (size > storageLimit * 0.75) {
+    //   //console.log(size);
+    //   var oldSize = size;
+    //   //console.log(oldSize);
+    //   //console.log(storage);
+    //   resize(oldSize, storage);
+    //   //console.log(result.retrieve('hahahah'))
+    //   return;
+    // }
   };
 
-  result.retrieve = function(/*...*/ 
-) {
-    // TODO: implement `retrieve`
+  result.retrieve = function(str) {
+    var index = getIndexBelowMaxForKey(str, storageLimit);
+    var bucket = storage[index];
+    if (!bucket) {
+      return undefined;
+    } else {
+      var ele;
+      for (var i = 0; i < bucket.length; i++) {
+        var currentTuple = bucket[i];
+        if (currentTuple[0] === str) {
+          ele = currentTuple[0];
+        }
+      }
+    }
+    return ele;
   };
 
-  result.remove = function(/*...*/ 
-) {
-    // TODO: implement `remove`
+  result.remove = function(str) {
+    var index = getIndexBelowMaxForKey(str, storageLimit);
+    var bucket = storage[index];
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === str) {
+        var currIndex = i;
+        bucket.splice(currIndex, 1);
+        size -= 1;
+        // if (size < storageLimit * 0.25) {
+        //   var oldSize = size;
+        //   result = result.resize(oldSize, result);
+        //   return;
+        // }
+        console.log(size);
+      }
+    }
   };
 
   return result;
 };
+var newHashTab = makeHashTable();
+newHashTab.insert('h', newHashTab);
+newHashTab.insert('y', newHashTab);
+newHashTab.insert('i', newHashTab);
+newHashTab.insert('o', newHashTab);
+newHashTab.remove('o');
+console.log(newHashTab.retrieve('o'));
