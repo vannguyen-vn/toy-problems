034919a284dc9a44de635f27c5158ae8f0684bd8
Gabrielle Davidson <gdvdsn@gmail.com>
2021-08-13 17:21:42 -1000

    Made sure function is in constant time

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -20,20 +20,33 @@
  var Stack = function() {
   var stack = Object.create(Stack.methods);
   stack.storage = [];
+  stack.minTracker = {};
   return stack;
 };
 
 Stack.methods = {
   push: function(value) {
     this.storage.push(value);
+    if (Object.keys(this.minTracker).length === 0) {
+      this.minTracker.min = value;
+    } else {
+      if (value < this.minTracker.min) {
+        this.minTracker.min = value;
+      }
+    }
   },
   pop: function() {
-    return this.storage.pop();
+    var popped = this.storage.pop();
+    var last = this.storage[this.storage.length - 1];
+    if (last > popped) {
+      this.minTracker.min = last;
+    }
+    return popped;
   },
   size: function() {
     return this.storage.length;
   },
   min: function() {
-    return Math.min(...this.storage);
+    return this.minTracker.min;
   }
 };
