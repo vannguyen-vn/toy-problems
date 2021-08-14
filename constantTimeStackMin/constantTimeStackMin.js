d05bf02932894a6e218dae77ac90abcd868cb065
Joseph Moon <joemoondev@gmail.com>
2021-08-13 19:20:12 -0700

    Initial commit, does not handle case when pop() returns min value

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -1,6 +1,6 @@
 /**
  * Write a stack using your preferred instantiation pattern. Implement a min function
- * that returns the minimum value of all the elements in the stack in constant time.stack.  
+ * that returns the minimum value of all the elements in the stack in constant time.stack.
 
  * All of the functions in the Stack should run in constant time!
  *
@@ -18,23 +18,33 @@
   * Stack Class
   */
   var Stack = function() {
+    this._index = 0;
+    this._storage = {};
+    this._min = Number.MAX_SAFE_INTEGER;
 
   // add an item to the top of the stack
     this.push = function(value) {
+      this._min = Math.min(value, this._min);
+      this._storage[String(this._index)] = value;
+      this._index += 1;
     };
 
   // remove an item from the top of the stack
     this.pop = function() {
+      var res = this._storage[String(this._index - 1)];
+      delete this._storage[String(this._index - 1)];
+      this._index -= 1;
+      return res;
     };
 
   // return the number of items in the stack
     this.size = function() {
+      return this._index;
     };
-  
+
   // return the minimum value in the stack
     this.min = function() {
-
+      return this._min;
     };
 
   };
-
