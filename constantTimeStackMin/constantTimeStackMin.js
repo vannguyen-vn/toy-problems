c63d98a7ee750dcf63bb0d7697d620355c7f7178
lucyjunejack <gkvoid3@gmail.com>
2021-08-13 19:15:30 -0700

    Assignment Completed

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -1,6 +1,6 @@
 /**
  * Write a stack using your preferred instantiation pattern. Implement a min function
- * that returns the minimum value of all the elements in the stack in constant time.stack.  
+ * that returns the minimum value of all the elements in the stack in constant time.stack.
 
  * All of the functions in the Stack should run in constant time!
  *
@@ -18,23 +18,31 @@
   * Stack Class
   */
   var Stack = function() {
+    var stack = [];
+    var size = 0;
+    var min = Number.MAX_VALUE;
 
   // add an item to the top of the stack
     this.push = function(value) {
+      if (value < min) {
+        min = value;
+      }
+      stack.unshift(value);
     };
 
   // remove an item from the top of the stack
     this.pop = function() {
+      return stack.shift();
     };
 
   // return the number of items in the stack
     this.size = function() {
+      return size;
     };
-  
+
   // return the minimum value in the stack
     this.min = function() {
-
+      return min;
     };
-
   };
 
