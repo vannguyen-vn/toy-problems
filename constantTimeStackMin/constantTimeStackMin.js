87fc5e32fa851022c422e69375d830c1e5a5375b
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-08-13 20:02:34 -0700

    Implemented push, pop, min, size. Refactoring for constant time (WIP)  - Reached time limit.

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -1,6 +1,6 @@
 /**
  * Write a stack using your preferred instantiation pattern. Implement a min function
- * that returns the minimum value of all the elements in the stack in constant time.stack.  
+ * that returns the minimum value of all the elements in the stack in constant time.stack.
 
  * All of the functions in the Stack should run in constant time!
  *
@@ -17,24 +17,64 @@
 /**
   * Stack Class
   */
-  var Stack = function() {
+  class Stack {
+
+   constructor (value) {
+    this.storage = {}
+    this.length = 0
+    this.minimum = null;
+    };
 
   // add an item to the top of the stack
-    this.push = function(value) {
+   push(value) {
+     this.storage[this.length] = value
+
+     if (this.length === 0) {
+      this.minimum = value;
+     } else if (this.minimum > value) {
+      this.minimum = value;
+     }
+     this.length++;
     };
 
   // remove an item from the top of the stack
-    this.pop = function() {
+    pop() {
+      delete this.storage[this.size -1]
+      this.length --;
+
+      if (this.length === 0) {
+        this.minimum = null;
     };
 
   // return the number of items in the stack
-    this.size = function() {
+   size() {
+     return this.length;
     };
-  
-  // return the minimum value in the stack
-    this.min = function() {
 
+  // return the minimum value in the stack
+    min() {
+      return this.minimum;
     };
 
   };
 
+
+  var example = new Stack()
+  example.push(4)
+  example.push(3)
+  console.log(example.min() === 3)
+  example.push(3)
+  example.push(2)
+  example.push(2)
+  console.log(example.min() === 2)
+
+  console.log(example)
+  example.pop();
+
+
+  console.log(example)
+
+  example.push(1)
+  console.log(example)
+  example.pop(1)
+  console.log(example)
