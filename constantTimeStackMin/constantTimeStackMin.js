6c36be92dac123ffa2dee032e6e6c91a634cb3af
Kevin Gao <kevinzhugao@gmail.com>
2021-08-13 19:12:05 -0700

    Implented a stack with several methods

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -1,6 +1,6 @@
 /**
  * Write a stack using your preferred instantiation pattern. Implement a min function
- * that returns the minimum value of all the elements in the stack in constant time.stack.  
+ * that returns the minimum value of all the elements in the stack in constant time.stack.
 
  * All of the functions in the Stack should run in constant time!
  *
@@ -17,24 +17,32 @@
 /**
   * Stack Class
   */
-  var Stack = function() {
+ var Stack = function() {
+  this.storage = [];
+  this.smallest;
 
-  // add an item to the top of the stack
-    this.push = function(value) {
-    };
-
-  // remove an item from the top of the stack
-    this.pop = function() {
-    };
-
-  // return the number of items in the stack
-    this.size = function() {
-    };
-  
-  // return the minimum value in the stack
-    this.min = function() {
-
-    };
-
+// add an item to the top of the stack
+  this.push = function(value) {
+    this.storage.push(value);
+    this.smallest = value;
   };
+
+// remove an item from the top of the stack
+  this.pop = function() {
+    this.storage.pop();
+  };
+
+// return the number of items in the stack
+  this.size = function() {
+    return this.storage.length;
+  };
+
+// return the minimum value in the stack
+  this.min = function() {
+    this.storage.forEach(value => {
+      if (value < this.smallest) { this.smallest = value; }
+    });
+    return this.smallest;
+  };
+};
 
