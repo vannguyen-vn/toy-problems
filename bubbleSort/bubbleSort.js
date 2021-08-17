51ab2c3ac65eb124feae64d527a97e2e15f00fb5
Iris Huang <yilu.huang@gmail.com>
2021-08-16 19:05:23 -0700

    Completed

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -34,5 +34,21 @@
 
 
 var bubbleSort = function(array) {
-  // Your code here.
+  var isSorted = true;
+  for (var i = 0; i < (array.length - 1); i++) {
+    if (array[i] > array[i + 1]) {
+      var temp = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = temp;
+      isSorted = false;
+    }
+  }
+
+  if (isSorted) {
+    return array;
+  } else {
+    return bubbleSort(array);
+  }
 };
+
+
