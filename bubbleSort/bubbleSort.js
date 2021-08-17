e33b7187d8cbd046bc849bee995be30d37e8b6b4
huongtran1993 <huongtran1993@gmail.com>
2021-08-16 20:05:01 -0700

    completed bubbleSort prompt

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -32,7 +32,85 @@
 
 // Feel free to add helper functions if needed.
 
+var swap = function (array, i) {
+  var oldVal = array[i];
+  array[i] = array[i + 1];
+  array[i + 1] = oldVal;
+};
+
+var sort = function(array, lastSwapPos) {
+  lastSwapPos = lastSwapPos || array.length - 1;
+  var currSwapPos;
+  array.forEach(function(item, i, array) {
+    if(i < lastSwapPos && item > array[i + 1]) {
+      swap(array, i);
+      currSwapPos = i;
+    }
+  });
+  if(!currSwapPos) {
+    return 0;
+  } else {
+    return currSwapPos;
+  }
+};
 
 var bubbleSort = function(array) {
-  // Your code here.
+  var lastSwapPos = sort(array);
+  while (lastSwapPos !== 0) {
+    lastSwapPos = sort(array, lastSwapPos);
+  }
+  return array;
 };
+
+//Time Complexity: O(n^2)
+
+/*Test
+var input = [2, 1, 3, 12, 1341, 231, 31, 1, 3, 4, 5, 7, 9];
+console.log('input: ' + input);
+var output = bubbleSort(input);
+var inputCopy = input.slice();
+var expected = inputCopy.sort(function(a, b) {
+  return a - b;
+});
+console.log('output: ' + output);
+console.log(JSON.stringify(output) === JSON.stringify(expected)); //true
+
+var array = function(n) {
+  var array = [];
+  for (var i = 0; i < n; i++) {
+    var num = Math.floor(Math.random() * 2000);
+    array.push(num);
+  }
+  return array;
+}
+
+var input = array(10)
+console.log('input: ' + input);
+var output = bubbleSort(input);
+var inputCopy = input.slice();
+var expected = inputCopy.sort(function(a, b) {
+  return a - b;
+});
+console.log('output: ' + output);
+console.log(JSON.stringify(output) === JSON.stringify(expected)); //true
+
+var input = array(20)
+console.log('input: ' + input);
+var output = bubbleSort(input);
+var inputCopy = input.slice();
+var expected = inputCopy.sort(function(a, b) {
+  return a - b;
+});
+console.log('output: ' + output);
+console.log(JSON.stringify(output) === JSON.stringify(expected)); //true
+
+var input = array(50)
+console.log('input: ' + input);
+var output = bubbleSort(input);
+var inputCopy = input.slice();
+var expected = inputCopy.sort(function(a, b) {
+  return a - b;
+});
+console.log('output: ' + output);
+console.log(JSON.stringify(output) === JSON.stringify(expected)); //true
+*/
