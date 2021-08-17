9caafc357269dbb897cf88f36a9d37eb8afe75d9
lucyjunejack <gkvoid3@gmail.com>
2021-08-16 19:12:56 -0700

    toy completed

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -35,4 +35,13 @@
 
 var bubbleSort = function(array) {
   // Your code here.
+  for (var i = 0; i < array.length - 1; i++) {
+    for (var j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
 };
