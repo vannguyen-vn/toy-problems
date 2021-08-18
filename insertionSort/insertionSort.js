16aeb879e2c58b61d6214386337358961ff6781c
Ya Liu <sarahl0216@gmail.com>
2021-08-17 20:42:34 -0700

    modified testingTransform

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -38,7 +38,7 @@
 var testingTransform = function(array) {
   var transform = [];
 
-  for (var i = 0; i < array.length - 1; i++) {
+  for (var i = 0; i < array.length; i++) {
     transform.push({value: array[i], i: i});
   }
 
