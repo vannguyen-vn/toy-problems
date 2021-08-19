4c353330106701c99216e691ded8ded353d13496
Aaron Tran <altran32@gmail.com>
2021-08-18 20:45:47 -0700

    delete pseudo and fix typo in loop

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -12,34 +12,22 @@
 
 
 var quicksort = function (array) {
-  //constraints
-  // if array.length is 0 or undefined return null;
   if (array.length === 0) {
     return [];
   }
-  // if array length is 1 return array
   if (array.length === 1) {
     return array;
   }
-  // declare pivot as 0 index
   let pivotVal = array[0]
-  // declare variable as lessArr
   let lessArr = [];
-  // declar variable as moreArr
   let moreArr = [];
-  // iterate over array
   for (let i = 1; i < array.length; i++) {
-    // if element is less than pivot val
     if (array[i] < pivotVal) {
-      // push less
       lessArr.push(array[i])
-      // otherwise
     } else {
-      // push morearr
       moreArr.push(array[i])
     }
   }
-  // recursively call and concat arrays
   return [quicksort(lessArr), pivotVal, quicksort(moreArr)].flat();
 
 };
