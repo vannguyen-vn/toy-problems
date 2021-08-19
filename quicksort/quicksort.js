1b1e84052c5f1b07456f42a977e65bc4c3c230e4
Qinyu Zhu <zz635186056@gmail.com>
2021-08-18 20:00:40 -0700

    first attempt

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -10,6 +10,27 @@
  *    Combines the two arrays and the pivot into a sorted array.
  */
 
-
 var quicksort = function(array) {
+  while (array.length > 0) {
+    partition(array);
+    array.splice(0, 1);
+  }
 };
+
+var partition = function(toSort) {
+  //helper function sort the array
+  var smaller = [];
+  var larger = [];
+  for (var i = 1 ;i < toSort.length; i++) {
+    if (toSort[i] < toSort[0]) {
+      smaller.push(toSort[i])
+    } else {
+      larger.push(toSort[i]);
+    }
+  }
+  // if smaller and larger are both single array, stop the recursion
+  // else , smaller array + pivot + larger array
+  smaller = smaller.push(toSort[0]);
+  return smaller.concat(larger);
+};
+
