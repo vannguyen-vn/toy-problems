699599b0463920cf3b81041432491958882008b6
Alex Romero <alexromeroreyes09@gmail.com>
2021-08-18 20:07:51 -0700

    Quicksort Toy Problem Submission

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -12,4 +12,29 @@
 
 
 var quicksort = function(array) {
+  // Create a pivot variable
+  var pivot = array[0];
+  // Create a smaller array
+  var smallerVals = [];
+  // Create a larger array
+  var largerVals = [];
+  // Check if array is less than or equal to zero
+  if (array.length <= 0) {
+    // If so return array
+    return array;
+  }
+  //Iterate over the array
+  for (var i = 1; i < array.length; i++) {
+    // If array at pivot is greater than value at i
+    if (pivot >= array[i]) {
+      //Push into smaller array
+      smallerVals.push(array[i]);
+      // Else if array at pivot is lesser than value at i
+    } else {
+      // Push into larger array
+      largerVals.push(array[i]);
+    }
+  }
+  // Combine smaller array, pivot, and larger array while passing into function
+  return quicksort(smallerVals).concat(pivot, quicksort(largerVals));
 };
