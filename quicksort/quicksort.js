4c0921f740149115ae9afaf7dc5a3888a043a750
Derek Samms <derek.samms@gmail.com>
2021-08-18 19:34:53 -0700

    recursion is working with one test, about to try some others

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -12,4 +12,41 @@
 
 
 var quicksort = function(array) {
+  //base case
+  //  when array length is 1
+  //    return that one?
+  //
+
+  if(array.length <= 1) {
+    return array;
+  }
+
+  //pick random index
+  // var randomIndex = Math.floor(Math.random * array.length);
+  var pivot = array[0];
+  var lesserValues = [];
+  var greaterValues = [];
+
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] <= pivot) {
+      lesserValues.push(array[i]);
+    } else {
+      greaterValues.push(array[i]);
+    }
+  }
+
+  var sortedLesserValues = quicksort(lesserValues);
+  var sortedGreaterValues = quicksort(greaterValues);
+  sortedLesserValues.push(pivot);
+  return sortedLesserValues.concat(sortedGreaterValues);
+  //create two result arrays (also save pivot)
+  //  sort into the two based on pivot
+  //  perform quicksort recursively on each slice
+  //  combine lowerSlice, pivot, upperSlice together into one result
+
+
+  //
 };
+
+var test1 = [4, 5, 2, 7, 1, 9, 0, 11]
+console.log(quicksort(test1), 'should equal', [0, 1, 2, 4, 5, 7, 9, 11])
