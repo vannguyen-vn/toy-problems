b819012bb4623e61753f0b502aad11965eb9bb0b
Caleb Kim <kim.caleb10@gmail.com>
2021-08-18 19:49:29 -0700

    Implemented quicksort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -9,7 +9,47 @@
  *    Recursively sorts the two halves.
  *    Combines the two arrays and the pivot into a sorted array.
  */
+var recursion = (array) => {
+  // create an isSorted, set to false
+  var usedSort = false;
+  // iterate over the array
+  for(let i = 1; i < array.length; i++) {
+    // if value at index 1 is less than index 0
+    if (array[i] < array[i - 1]) {
+      // swap the two values
+      var swappings = array[i];
+      array[i] = array[i - 1];
+      array[i - 1] = swappings;
+      // isSorted true
+      usedSort = true;
+    }
+  }
+  // if isSorted
+  if (usedSort) { recursion(array) }
+    // recursionLess(array)
+  // return array
+  return array;
+}
 
-
-var quicksort = function(array) {
+var quicksort = (array) => {
+  // create two arrays, less and more array
+  var arrayCopy = array.slice();
+  var less = [];
+  var more = [];
+  // create pivot value in the array, using Math.random and multiplying by array length splice out of array
+  var pivotIndex = Math.floor(Math.random() * array.length);
+  var pivotValue = arrayCopy.slice(pivotIndex, 1);
+  // iterate over the array
+  for(let i = 0; i < arrayCopy.length; i++) {
+    // if index is less than pivot value push to less else push to more
+    arrayCopy[i] < pivotValue ? less.push(arrayCopy[i]) : more.push(arrayCopy[i]);
+  }
+  // use a less and more recursive function for both less and more
+  recursion(less);
+  recursion(more);
+  // push pivot value into less and then concat with more
+  less.push(pivotValue);
+  const sorted = less.concat(more);
+  // return sorted array
+  return sorted;
 };
