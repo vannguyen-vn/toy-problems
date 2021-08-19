4e6f4b8007f9de629ff09c06be93fcc8d635e341
Gabrielle Davidson <gdvdsn@gmail.com>
2021-08-18 17:36:33 -1000

    Completed quicksort toy problem

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -11,5 +11,46 @@
  */
 
 
-var quicksort = function(array) {
+ var quicksort = function(array) {
+  if (array.length === 0) {
+    return [];
+  }
+
+  var split = [[], []];
+  var lessThanPivot = split[0];
+  var greaterThanPivot = split[1];
+  var pivot;
+
+  if (array.length % 2 === 1) {
+    pivot = array[(array.length - 1) / 2];
+  } else {
+    pivot = array[array.length / 2];
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === pivot && array.length > 1) {
+      continue;
+    } else if (array[i] === pivot) {
+      return array;
+    } else if (array[i] < pivot) {
+      lessThanPivot.push(array[i]);
+    } else {
+      greaterThanPivot.push(array[i]);
+    }
+  }
+
+  if ((lessThanPivot.length === 1 || lessThanPivot.length === 0) && (greaterThanPivot.length === 1 || greaterThanPivot.length === 0)) {
+    return lessThanPivot.concat(pivot, greaterThanPivot);
+  }
+
+  var sorted = [];
+
+  for (var i = 0; i < split.length; i++) {
+    if (i === 1) {
+      sorted.push(pivot);
+    }
+    sorted = sorted.concat(quicksort(split[i]));
+  }
+
+  return sorted;
 };
