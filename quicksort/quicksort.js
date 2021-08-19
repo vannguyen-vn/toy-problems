2b86394a6d6120afd4ca6fed4e40afe43f9647e8
Andy Fang <afang1094@gmail.com>
2021-08-18 19:57:51 -0700

    first attempt

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -11,5 +11,42 @@
  */
 
 
-var quicksort = function(array) {
+var quicksort = function (array) {
+  // declare var for pivot and send to random number in array
+  var pivot = array[Math.floor(Math.random() * array.length)];
+  // console.log(pivot);
+
+  // declare smaller array
+  var small = [];
+  // declare larger array
+  var large = [];
+
+  // declare inner function
+  var innerFunc = function (num) {
+    // check if current is smaller then pivot
+    if (num <= pivot) {
+      // push to smaller
+      small.push(num);
+      // else
+    } else {
+      // push to bigger
+      large.push(num);
+    }
+    return;
+  }
+
+  // iterate over array
+  for (var i = 0; i < array.length; i++) {
+    // recursive call inner function
+    innerFunc(array[i]);
+  }
+  // call inner function
+  innerFunc();
+
+  // return result
+  return small.concat(large);
+
 };
+
+
+// console.log(quicksort([4, 6, 8, 2, 1, 3]))
