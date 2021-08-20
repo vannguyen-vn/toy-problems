85173dd329d6edfb610bba10b98182cc4b7ca028
Bishal Gautam <bgautam.se@gmail.com>
2021-08-19 20:05:19 -0700

    merge sort: ive done this one before :o

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -97,6 +97,48 @@
 
 
 
-var mergeSort = function(array) {
-  // Your code here.
-};
+
+var mergeSort = (array) => {
+  //base case, if we an array length 1 or 0, return
+  if (array.length <= 1) return array;
+
+  var midPoint = Math.floor(array.length/2);
+  var leftArray = array.slice(0, midPoint);
+  var rightArray = array.slice(midPoint);
+
+  //invoke recursively on the left array, and the right array, and then merge the two sorted arrays
+  var sortLeft = mergeSort(leftArray);
+  var sortRight = mergeSort(rightArray);
+  var merged = merge(sortLeft, sortRight)
+  return merged;
+
+}
+
+
+var merge = (leftArray, rightArray) => {
+
+  var merged = [];
+  var leftPointer = 0;
+  var rightPointer = 0;
+  //have a starting pointer for each array   [1*, 2, 3]  [2*, 3, 5]
+  //continue the loop until any one pointer has reached the end of either array
+  while ( leftPointer < leftArray.length && rightPointer < rightArray.length) {
+
+      var leftVal = leftArray[leftPointer];
+      var rightVal = rightArray[rightPointer];
+    //compare the values at the left and right pointer
+    //push the lesser value into the merged array and move its pointer forward
+      if (leftVal < rightVal) {
+        merged.push(leftVal);
+        leftPointer++;
+      } else {
+        merged.push(rightVal);
+        rightPointer++
+      }
+  }
+    //spread operator to the rescue! it is not possible to know which array will have left over items, so we can just slice according to the pointer index
+  //either the leftArr or the rightArr will be empty, but the array will come out merged, and sorted
+  return [...merged, ...array.slice(leftPointer), ...array.slice(rightPointer)]
+
+
+}
