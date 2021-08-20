2a198527f63d295e4f5bf910e8b20461eb7caa8e
Iris Huang <yilu.huang@gmail.com>
2021-08-19 20:26:10 -0700

    Updated quick sort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -13,26 +13,29 @@
 
 var quicksort = function(array) {
   var j = 0;
-  var isSorted = true;
   var pivot = array[array.length - 1];
+
+  if (array.length === 1) {
+      return array;
+  }
   for (var i = 0; i < array.length; i++) {
     if (i === array.length - 1) {
-      if (isSorted) {
-        return array;
-      } else {
         var replace = array[j];
         array[j] = pivot;
         array[array.length - 1] = replace;
-        return quicksort(array.slice(0, j)).concat(quicksort(array.slice(j)));
+        if (j === 0) {
+          return array.slice(0, 1).concat(quicksort(array.slice(j + 1)));
+        } else {
+          return quicksort(array.slice(0, j)).concat(quicksort(array.slice(j)));
+        }
       }
-    } else if (array[i] <= pivot) {
+
+    if (array[i] <= pivot) {
       var temp = array[i];
       array[i] = array[j];
       array[j] = temp;
       j++;
-      isSorted = false;
     }
   }
 };
-
 
