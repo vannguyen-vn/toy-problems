052c615963e2783f09b7390d41221de9cfb15d9e
Van Nguyen <van.nguyen@texo.design>
2021-08-23 20:43:13 -0700

    Completed: firstNonRepeatedCharacter

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,26 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
+/*
+input: string
+output: the char( the first nonrepeated char)
+
+Pseudocode
+- Go though the string
+  - create var char to store the value of string[i]
+  - if the index the curr character and index the next character = -1
+  - return curr character
+return null
+
+ */
+
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  for (var i = 0; i < string.length; i++) {
+    var char = string.charAt(i);
+    if (string.indexOf(char) && string.indexOf(char, i+1) === -1) {
+      return char;
+    }
+  }
+  return null;
 };
+
