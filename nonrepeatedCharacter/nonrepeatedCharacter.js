0385e9a99003b22ba5fe787f60424abf31820703
Jandean Deocampo <jandeo92@gmail.com>
2021-08-23 19:22:09 -0700

    Attempt 2 at nonrepeatedCharacter.

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,11 +8,14 @@
 
 var firstNonRepeatedCharacter = function(string) {
   var uniques = new Set();
+  var deleted = new Set();
 
   for (var i = 0; i < string.length; i++) {
     let ch = string[i];
+    if (deleted.has(ch)) { continue; }
     if (uniques.has(ch)) {
       uniques.delete(ch);
+      deleted.add(ch);
       continue;
     }
     uniques.add(ch);
