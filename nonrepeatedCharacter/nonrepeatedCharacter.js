14f368e99ced9d0f1b7fa3fe93027066b564a7fd
Ryan Rhoads <rhoads.ryan.grant@gmail.com>
2021-08-23 20:03:19 -0700

    Assignment Complete

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,29 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  // Storage Object
+  var results = {};
+  // Iterate over entire array
+  for (var i = 0; i < string.length; i++) {
+    // If first set to 1
+    if(results[string[i]] === undefined) {
+      results[string[i]] = 1;
+    }
+    // Increment the counter in results
+    else {
+      results[string[i]]++;
+    }
+  }
+  // Iterate over results object and return first value equal to 1
+  for (var element in results) {
+    if (results[element] === 1) {
+      return element;
+    }
+  }
 };
+
+// var test1 = firstNonRepeatedCharacter('ABA'); // => 'B'
+// var test2 = firstNonRepeatedCharacter('AACBDB'); // => 'C'
+
+// console.log('Test 1: ' + test1);
+// console.log('Test 2: ' + test2);
