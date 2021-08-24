b68fde7ef4e524b640f67b1fae4421035a7199d8
John Fa <john6fa@gmail.com>
2021-08-23 19:09:15 -0700

    non repeated character attemp

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,36 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+/*
+create hash map
+key: letter
+value: frequency of the letter
+loop through string
+  count frequency
+
+loop through hash map
+  check which key is first mapped to 1
+  return the character at that key
+
+
+*/
+
+
+var firstNonRepeatedCharacter = function (string) {
+  const map = new Map();
+  for (var i = 0; i < string.length; i++) {
+    if (map.has(string[i])) {
+      map.set(string[i], (map.get(string[i]) + 1));
+    } else {
+      map.set(string[i], 1);
+    }
+  }
+
+  for (var [key, value] of map.entries()) {
+    if (value === 1) {
+      return key;
+    }
+  }
+
+  return null;
 };
