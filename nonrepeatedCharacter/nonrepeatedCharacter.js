f2b8f13798765186a51237eb6c7d00ec810fd94f
Bishal Gautam <bgautam.se@gmail.com>
2021-08-23 19:42:41 -0700

    first non repeat char

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,26 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+ var firstNonRepeatedCharacter = function(string) {
+  string = string.toLowerCase();
+  //question didn't specifiy casing
+
+  var counts = {};
+  for (var i = 0; i < string.length; i++) {
+    if (!counts[string[i]]) {
+      counts[string[i]] = 1;
+    } else {
+      counts[string[i]]++;
+    }
+  }
+  for (var i = 0; i < string.length; i++) {
+    if (counts[string[i]] === 1) {
+      return string[i];
+    }
+  }
+  return -1;
+
 };
+
+//two for loops, O(2n) --> O(n)
+
