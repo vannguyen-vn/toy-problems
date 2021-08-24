21c4c82cb3b4b40f06ca2ae4c92e94e9327dabbb
Derek Samms <derek.samms@gmail.com>
2021-08-23 19:09:46 -0700

    implemented an idea; testing

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,20 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  // iterate across, at each letter:
+    // iterate, looking for a match
+    //  if there's a match, splice it AND THE MATCH away?
+  // OR...
+  //
+  for (var i = 0; i < string.length; i ++) {
+    var repeated = false;
+    for (var j = i + 1; j < string.length; j++) {
+      if (string[j] === string[i]) {
+        repeated = true;
+      }
+    }
+    if (!repeated) {
+      return string[i];
+    }
+  }
 };
