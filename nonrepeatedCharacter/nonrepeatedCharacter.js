c8f556124ba4a28fb9148209be8e65c973429c0d
Alex Romero <alexromeroreyes09@gmail.com>
2021-08-23 19:29:14 -0700

    Non Repeated Character Toy Problem Submission

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,29 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var lowerString = string.toLowerCase();
+  // Create a counter object
+  var counterObj = {};
+  // Iterate over the input string
+  for (var i = 0; i < lowerString.length; i++) {
+  // For every letter check if it exists in the obj
+    if (counterObj[lowerString[i]] = counterObj[lowerString[i]]) {
+      // If so
+      // Increment value by one
+      counterObj[lowerString[i]]++;
+      console.log(counterObj);
+       // Otherwise
+    } else {
+      // Initiate value at key to 1
+      counterObj[lowerString[i]] = 1;
+      console.log(counterObj);
+    }
+  }
+  // Check object values
+  for (key in counterObj) {
+    // return key with value of 1
+    if (counterObj[key] === 1) {
+      return key;
+    }
+  }
 };
