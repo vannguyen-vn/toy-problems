27b60d99e88c5b405182a83fae06d5d111d1d351
Lenora Esquenazi <lenaciousd@gmail.com>
2021-08-23 19:13:52 -0700

    Planned and have examples ready to be logged

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,27 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
+// input: string
+// output: string - 1st char not repeated
+// constraints: none
+// edge cases: if all repeated, empty str - return empty str
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var letterCounts = {}
+  // loop through str
+    // for each
+    // if not already a key in letterCounts
+      // add it, with value 1
+    // otherwise
+      // increment value by 1
+
+    // loop through str again
+      // for each
+      // if the object at that letter has a value of 1
+        // return
 };
+
+// Example:
+var test1 = firstNonRepeatedCharacter('ABA'); // => 'B'
+console.log(test1)
+var test2 = firstNonRepeatedCharacter('AACBDB'); // => 'C'
+console.log(test2)
