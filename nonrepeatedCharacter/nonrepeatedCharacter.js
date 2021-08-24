b7e0bd7cb06ff69d815b81312d40e3ac65818409
Andrew Cho <andrewgunncho@gmail.com>
2021-08-23 19:36:02 -0700

    Completed 'nonrepeatedCharacter'

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,29 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+ var firstNonRepeatedCharacter = function(string) {
+  var container = [];
+  var obj = {};
+
+  for (var i = 0; i < string.length; i++) {
+    if (obj[string[i]] === undefined) {
+      obj[string[i]] = 1;
+    } else {
+      obj[string[i]]++;
+    }
+  }
+
+  for (key in obj) {
+    if (obj[key] === 1) {
+      container.push(key);
+    }
+  }
+
+  for (var i = 0; i < string.length; i++) {
+    for (var j = 0; j < container.length; j++) {
+      if (string[i] === container[j]) {
+        return container[j];
+      }
+    }
+  }
 };
