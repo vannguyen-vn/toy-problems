d1c7299c05e0a2ab4a7e453a639fb54239bc16dd
Matthew Boyle <matthewtboyle89@gmail.com>
2021-08-23 19:12:05 -0700

    finished assignment

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -5,7 +5,23 @@
  *   firstNonRepeatedCharacter('ABA'); // => 'B'
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
+// input string of characters
+// output: the character that was repeated first
+
+
+// one way to do it is to put characters in an object
+// if they don't exist, give them a value of one
+// if they do exist, return that character
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var charStorage = {};
+  for (var i = 0; i < string.length; i++) {
+    var char = string[i];
+    if (!charStorage[char]) {
+      charStorage[char] = 1;
+    } else {
+      return char;
+    }
+  }
 };
