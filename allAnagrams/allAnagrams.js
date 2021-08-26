faad60c43d78613635808e9d36bbf9aa5b03eec0
Caleb Kim <kim.caleb10@gmail.com>
2021-08-25 19:22:59 -0700

    Implemented allAnagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -14,4 +14,30 @@
 
 var allAnagrams = function(string) {
   // Your code here.
+  // create a results array
+  const list = [];
+  // split string into an array
+  const letters = string.split('');
+  // create anagram variable
+  const anagram = '';
+  // create recursive function
+  const anagramCreator = (anagram) => {
+    // base case
+    // if anagram.length === string.length, push anagram to results array, return
+    if (anagram.length === string.length) { list.push(anagram); return; }
+    // iterate over the string array
+    for (let i = 0; i < letters.length; i++) {
+      // call recursion with anagram + stringarray[i]
+      if (anagram.indexOf(letters[i]) !== -1) {
+        continue;
+      } else {
+        anagramCreator(anagram + letters[i]);
+      }
+    }
+  }
+  // call recursive function with anagram varaiable
+  anagramCreator(anagram);
+  // return results array
+  // _.uniq(list);
+  return list;
 };
