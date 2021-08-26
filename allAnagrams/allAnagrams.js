625f1f90a0087fb450c2b997ed3c282c0f176ff7
Van Nguyen <van.nguyen@texo.design>
2021-08-25 20:19:41 -0700

    Completed: allAnagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,23 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+/*
+input : string
+output: array (anagrams of a string)
+*/
+
 var allAnagrams = function(string) {
-  // Your code here.
+  var anaOutPut = {};
+  var helper = function (anaSofar, str) {
+    if (str === '') {
+      anaOutPut[anaSofar] = true;
+      return;
+    }
+   for ( var i = 0; i < str.length; i++) {
+    helper(anaSofar + str[i], str.slice(0,i) + str.slice(i+1));
+   }
+  }
+
+  helper('', string);
+  return Object.keys(anaOutPut);
 };
