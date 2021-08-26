4d9748b314b62fe12aee6bc5ba224ff087cfc86a
Derek Samms <derek.samms@gmail.com>
2021-08-25 19:25:36 -0700

    implemented an attempt; testing

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,35 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  var anagrams = [];
+  for (var i = 0; i < string.length; i ++) {
+    for (var n = 0; n < string.length; n++) {
+      anagrams.push(string[0]);
+    }
+    var remainingString = string.slice(i + 1);
+    var anagramsToAdd = allAnagrams(remainingString);
+    for (var j = 1; j < string.length; j++) {
+      anagrams[j] += anagramsToAdd[j];
+    }
+  }
+
+  return anagrams;
+
+  // var anagramBuilder = function(string, anagram);
+  // for (var i = 0; i < string.length; i++) {
+  //   var newAnagram = string[i]
+  //   var remainingString = string.slice(i + 1);
+  // }
+
+  //base
+  // end of letters (anagram.length === string.length)
+  //   that's the anagram: return it/push it into the result array
+
+
+  //recursive case
+  //  iterate across
+  //    start a string with each letter, push into an array (as many as length of string - 1)
+  //    iterate across that array
+  //      continue each string with each successive letter
+
 };
