2ada061c15b56b78d87e0dbd765284fce5643a3c
Cora Durham <cora5du@gmail.com>
2021-08-25 22:01:17 -0500

    Completed allAnagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -14,4 +14,21 @@
 
 var allAnagrams = function(string) {
   // Your code here.
+  var permutations = [];
+  var recursiveFunc = (currentPerm, remaining) => {
+    if (remaining.length === 0) {
+      permutations.push(currentPerm);
+      return;
+    }
+
+    for (var i = 0; i < remaining.length; i++) {
+      currentString = currentPerm + remaining[i];
+      recursiveFunc(currentString, remaining.substring(0, i) + remaining.substring(i + 1));
+    }
+
+
+  }
+  recursiveFunc('', string);
+  return permutations;
+
 };
