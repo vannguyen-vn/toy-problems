f0f2a9a8b9c7ad0c2304f9e862bddb2374d660d2
John Fa <john6fa@gmail.com>
2021-08-25 19:49:36 -0700

    all anagrams attempt

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -8,10 +8,51 @@
 
 /**
   * example usage:
-  * var anagrams = allAnagrams('abc');
-  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+   var anagrams = allAnagrams('abc');
+  console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
+/*
+if string length == 1
+  return string
+
+make results []
+call helper function with string, results, empty string, index
+return results
+
+helper function
+base case: if current length equals string length
+  push into results array
+
+for loop through string
+  add ith element in string to current
+  recursive call helper function, increment index
+  remove last character from current
+*/
+
+var allAnagrams = function (string) {
   // Your code here.
+  if (string.length === 1) {
+    return string;
+  }
+
+  var results = [];
+  helper(string, results, "");
+  return results;
 };
+
+var helper = function (string, results, current) {
+  if (current.length === string.length) {
+    results.push(current);
+    return;
+  }
+
+  for (var i = 0; i < string.length; i++) {
+    if (current.indexOf(string[i]) >= 0) {
+      continue;
+    }
+    current += string[i];
+    helper(string, results, current);
+    current = current.slice(0, -1);
+  }
+}
