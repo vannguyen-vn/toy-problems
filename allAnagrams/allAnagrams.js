e585850a5b92ac42a4ff3730dc414226199f6e65
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-08-25 20:14:22 -0700

    This is not the best implementation

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,30 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+ var allAnagrams = function(string) {
+
+  var anagrams = new Set();
+  var stringArray = string.split('');
+  var iterations = string.length;
+  var count = 100000;
+
+  var strings = '';
+  while (count > 0) {
+
+    var rIndex = Math.floor(Math.random() * stringArray.length);
+    strings += stringArray[rIndex];
+    stringArray.splice(rIndex, 1)
+
+    if (strings.length === string.length) {
+      anagrams.add(strings);
+      strings = '';
+      count--;
+      stringArray = string.split('');
+    }
+  }
+
+
+
+  return Array.from(anagrams);
 };
+
