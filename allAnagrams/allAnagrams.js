5e01216bd7e6127c602b1607b3a878b2c8683943
Alex Romero <alexromeroreyes09@gmail.com>
2021-08-25 20:05:19 -0700

    Toy Problem Submission

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,28 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  // Declare a result array
+  var results = [];
+  // Create inner recursive function that takes in a newString
+  var recursiveFunc = function(newString) {
+    // Base Case: If newString length equals string length
+    if(newString.length === string.length) {
+      // Push into results array
+      results.push(newString);
+        // Else
+    } else {
+      // Iterate over string
+      for (var i = 0; i < newString.length; i++) {
+        // splice the newString at the current index
+        newString.slice(i, 1);
+        // Call Recursive function on newString
+        recursiveFunc(newString + newString[i]);
+        newString.shift(newString[i]);
+      }
+    }
+  };
+  // Call Recursive function on empty string
+  recursiveFunc("");
+  // Return results
+  return results;
 };
