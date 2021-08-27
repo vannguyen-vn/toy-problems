98c426d9ae03b5ca0c3c166fd78e8cd345ad4267
Bishal Gautam <bgautam.se@gmail.com>
2021-08-26 20:19:34 -0700

    coins

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -26,6 +26,21 @@ makeChange(2) === 2
 
 var makeChange = function(total) {
 
+  var coins = [1,2,5,10,20,50,100,200];
+  var combos = new Array(total + 1).fill(0);
+  combos[0] = 1;
+
+  for (var i = 0; i < coins.length; i++) {
+    for (var j = 1; j < combos.length; j++) {
+      if (j >= coins[i]) {
+        combos[j] += combos[j-coins[i]];
+      }
+    }
+  }
+
+  // console.log(combos);
+  return combos[total];
 };
+
 
 
