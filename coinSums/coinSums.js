600f8272befa4ec6b6d23fc2a940bccfb5c9ef1a
Van Nguyen <van.nguyen@texo.design>
2021-08-26 21:21:53 -0700

    Completed: coinSums

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -22,10 +22,49 @@ example usage of `makeChange`:
 makeChange(1) === 1
 // aka, there's only two ways to make 2p. that's with two, 1p pieces or with a single 2p piece
 makeChange(2) === 2
-*/
+/*
+Input  = number (total)
+coins = [1,2,5,10,20,50,100,200]
+Output = array (return all possible decision paths)
+Pseudocode
+// create result = []
+// create makeChangeSofar = []
+// create a helper function to check: currentTotal, makeChangeSoFar
+  // If currTotal = total
+    // push total into result
+    // return
+  // If currTotal >  total
+    // return
+  // Go though the coins
+    // Move to next and do recursion
+    // With currentTotal += coins[i] and add coins[i] into makeChangeSoFar
+  // kick-off the recursive: with currentTotal = 0; coins, makeChangeSoFar = []
+// return reuslt;
+ */
+
 
 var makeChange = function(total) {
 
+  var coins = [1,2,5,10,20,50,100,200]
+  var result = [];
+  var makeChangeSoFar = [];
+
+  var makeChangeFunction = function(currentTotal, coins , makeChangeSoFar) {
+    if (currentTotal === total) {
+      result.push(makeChangeSoFar);
+      return;
+    }
+
+    if (currentTotal > total) {
+      return;
+    }
+    for ( var coin of coins) {
+      makeChangeFunction(currentTotal + coin, coins,makeChangeSoFar.concat(coin));
+    }
+  }
+  makeChangeFunction(0, coins, []);
+  return result;
 };
+
 
 
