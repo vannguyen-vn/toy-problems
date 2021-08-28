df5b0527c1c12898b53839f673f0193d2e2d825d
Andrew Cho <andrewgunncho@gmail.com>
2021-08-27 19:45:12 -0700

    Completed 'powerSet'

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,30 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
 var powerSet = function(str) {
+  var result = [''];
+
+  for (var i = 0; i < str.length; i++) {
+
+      var container = [];
+
+      for (var j = 0; j < result.length; j++) {
+
+          container.push(result[j] + str[i]);
+      }
+
+      result = result.concat(container);
+  }
+
+  return result;
 };
