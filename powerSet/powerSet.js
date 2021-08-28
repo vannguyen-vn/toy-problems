78e931d31df5543fcd64275ff162f60f4d7b1e77
Lenora Esquenazi <lenaciousd@gmail.com>
2021-08-27 20:17:59 -0700

    powerSet attempted

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,36 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
+// input: string
+// output: array of all combinations
+// constraints:
+// edge cases:
 var powerSet = function(str) {
+  // base case: empty str
+  if (str.length === 0) {
+    // return an array with an empty str
+    return ['']
+  }
+
+  var nextLetter = str[0]
+
+  var set = powerSet(str.slice(1))
+  return set.concat(set.map(currentWord => nextLetter + currentWord)).sort()
+
 };
+
+powerSet("abc")
+//-> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
+
+powerSet("jump")
+//-> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
