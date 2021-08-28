452c3fd83093fac5bce3b47ee8b40e0c435b555c
Van Nguyen <van.nguyen@texo.design>
2021-08-27 21:28:12 -0700

    Completed: powerSet

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,33 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function(str) {
-};
+/*
+Input: str;
+Output: array (all possible subsets including the empty set with all sorted characters)
+
+ */
+function powerSet(str) {
+  let result = []
+
+  function recursion(subSet, index) {
+    if (index >= str.length) {
+      result.push(subSet)
+      return
+    }
+    recursion(subSet, index + 1)
+    recursion(subSet + str[index], index + 1)
+  }
+
+  recursion("", 0)
+  return result.sort();
+}
