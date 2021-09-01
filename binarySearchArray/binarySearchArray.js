1a889749aa40669e065c460356f6fc952f4613af
Ya Liu <sarahl0216@gmail.com>
2021-08-31 19:49:06 -0700

    first attempt

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,39 @@
  */
 
 var binarySearch = function (array, target) {
+  var found = false;
+
+  var search = (arr, target) => {
+    if (arr.length === 0) {
+      return;
+    }
+
+    if (arr.length === 1) {
+      if (arr[0] === target) {
+        found = true;
+        return target;
+      }
+    }
+
+    if (arr.length > 1) {
+      var middleIndex = Math.floor(arr.length / 2);
+      var left = arr.slice(0, middleIndex);
+      var right = arr.slice(middleIndex);
+
+      if (target < right[0]) {
+        binarySearch(left, target);
+      } else {
+        binarySearch(right, target);
+      }
+
+      if (arr.indexOf(target) >= 0) {
+        found = true;
+        return target;
+      }
+    }
+  }
+  search(array, target);
+
+  return found ? array.indexOf(target) : null;
 };
 
