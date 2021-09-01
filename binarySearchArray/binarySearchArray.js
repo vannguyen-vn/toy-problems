0cd3bac36c1120c1810856171a8b46123d56c2a9
Bishal Gautam <bgautam.se@gmail.com>
2021-08-31 19:24:14 -0700

    binary search array

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,35 @@
  */
 
 var binarySearch = function (array, target) {
+
+  //have pointer at start, middle and end
+  //if middle = target, return middle
+  //if target is bigger than midpt, set start pointer to midpoint, find new midpoint
+  //if target is smaller than midpoint, set end pointer to midpoint, find new midpoint
+  var startIndex = 0;
+  var lastIndex = array.length - 1;
+  var midpoint = Math.floor((startIndex+lastIndex)/2);
+  for (var i = 0; i < array.length; i++) {
+    console.log(startIndex, lastIndex, midpoint, 'val:', array[midpoint]);
+    if (startIndex > lastIndex) {
+      return null;
+    }
+    if (array[midpoint] === target) {
+      return midpoint;
+    }
+    if (target > array[midpoint]) {
+      startIndex = midpoint;
+      midpoint = Math.floor((startIndex+lastIndex)/2);
+    }
+    if (target < array[midpoint]) {
+      lastIndex = midpoint;
+      midpoint = Math.floor((startIndex+lastIndex)/2);
+    }
+  }
+  return null;
+
 };
 
+
+// var array = [1, 4, 6 , 7, 9, 24, 24, 37, 60];
+// console.log(binarySearch(array, 8));
