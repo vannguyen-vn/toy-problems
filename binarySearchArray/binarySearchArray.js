a41182785cf80336eeb8bf4a4a6a08825166e19b
Daniel Prejs <danielnprejs@gmail.com>
2021-08-31 19:32:35 -0700

    binary search array attempt 1

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,31 @@
  */
 
 var binarySearch = function (array, target) {
+  debugger;
+  if (array.length === 1 && array[0] !== target) { return null; }
+  var arrayMiddle = Math.floor((array.length - 1)/2);
+  if (array[arrayMiddle] === target) {
+    return arrayMiddle;
+  } else if (array[arrayMiddle] > target) {
+    return binarySearch(array.slice(0, arrayMiddle), target)
+  } else {
+    var rightSearch = binarySearch(array.slice(arrayMiddle + 1), target);
+    if (rightSearch !== null) {
+      return rightSearch + arrayMiddle + 1;
+    } else {
+      return null;
+    }
+  }
+  // find middle of array
+  // compare target to array
+  // if equal
+    //return index
+  // if larger than target
+    // search left of the middle
+  // if smaller than target
+    // check right of middle
+
 };
+binarySearch([1, 2, 3, 4, 5], 4);
+module.exports = binarySearch;
 
