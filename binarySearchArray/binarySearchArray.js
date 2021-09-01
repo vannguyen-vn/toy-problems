18deab483e6721eadd28a971377135387ba20d14
Derek Samms <derek.samms@gmail.com>
2021-08-31 19:09:25 -0700

    began implementing some code for binarySearch

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,29 @@
  */
 
 var binarySearch = function (array, target) {
+  //takes in an array and a value; returns the index of the value using binary search
+  // find the middle value
+    // if the target is equal to the middle value
+      // return the middle value
+    // if the target is less than the middle value
+      // search the lower end (recursive)
+    // if the target is greater than the middle value
+      // search the higher end (recursive)
+
+  var middleIndex = Math.floor(array.length/2);
+  var middleValue = array[middleIndex];
+
+  if (array.length === 1 && array[0] !== target) {
+    return null;
+  }
+  if (middleValue === target) {
+    return middleIndex;
+  }
+  if (middleValue > target) {
+    // return binarySearch(lower end)
+  }
+  if (middleValue < target) {
+    // return binarySearch(higher end)
+  }
 };
 
