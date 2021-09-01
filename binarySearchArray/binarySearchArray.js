d917afa62c983fccecd0f4fa535d7aac2d3901da
Isabell Lee <izzy.isabell@gmail.com>
2021-08-31 20:26:57 -0700

    Implemented binarySearchArray

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,23 @@
  */
 
 var binarySearch = function (array, target) {
+  var midpoint = Math.floor(array.length / 2);
+  var pivot = array[midpoint];
+
+  if (target < array[0] || target > array[array.length - 1] ) {
+    return null;
+  } else if (target === pivot) {
+    return midpoint;
+  } else if (target < pivot) {
+    return binarySearch(array.slice(0, midpoint), target);
+  } else {
+    return midpoint + 1 + binarySearch(array.slice(midpoint + 1), target);
+  }
 };
 
+
+//// testing ////
+// var index = binarySearch([1, 2, 3, 4, 5, 7, 9, 11, 14, 16], 9);
+// console.log(index); // 3
+// var index = binarySearch([1, 2, 3, 4, 5], 8);
+// console.log(index); // null
