9f71caf6dbda9d8f827fcaae31688366c90f32e6
Caleb Kim <kim.caleb10@gmail.com>
2021-08-31 19:23:55 -0700

    Implemented BinarySearchArray

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,37 @@
  */
 
 var binarySearch = function (array, target) {
+  // create result === null
+  var result = null;
+  // create a recursive function
+  var binary = (array) => {
+    // find midpoint of current array
+    const midpoint = Math.floor(array.length / 2);
+    // split array at midpoint, designation left and right, lower and higher respectively
+    const left = array.slice(0, midpoint);
+    const right = array.slice(midpoint, array.length);
+    if (target > array[midpoint]) {
+      if (left.length === 0 || right.length === 0) {
+        return;
+      }
+      binary(right);
+    } else if (target < array[midpoint]) {
+      // run recursion using the left array
+      if (left.length === 0 || right.length === 0) {
+        return;
+      }
+      binary(left);
+    } else if (target === array[midpoint]) {
+      result = array[midpoint];
+      return;
+    }
+  }
+  binary(array);
+  return result === null ? result : array.indexOf(result);
 };
 
+var index = binarySearch([1, 2, 3, 4, 5], 4);
+console.log(index); // 3
+
+var index = binarySearch([1, 2, 3, 4, 5], 8);
+console.log(index); // null
