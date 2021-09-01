93b4d19a0aaff03cc716484edfb9849f3f459239
Van Nguyen <van.nguyen@texo.design>
2021-08-31 21:37:28 -0700

    Fixed error

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,19 +11,22 @@
  */
 
 var binarySearch = function (array, target) {
-  if (target > array[array.length - 1] || (target < array[0])) { return null;}
+  if (target > array[array.length - 1] || (target < array[0])) { return null; }
 
   var sub = function (lowIndex, highIndex) {
     if (lowIndex === highIndex) { return null; }
+    if (array.length === 1 && target !== array[0]) {
+      return null;
+    }
     var mid = Math.floor((highIndex - lowIndex) / 2) + lowIndex;
     if (target === array[mid]) {
       return mid;
     } else if (target < array[mid]) {
       return sub(lowIndex, mid);
-    } else if (target > array[mid])  {
-      return sub(mid, highIndex);
-    } else {
+    } else if (target > array[mid] && target < array[mid + 1]) {
       return null;
+    } else if (target > array[mid]) {
+      return sub(mid, highIndex);
     }
   }
 
