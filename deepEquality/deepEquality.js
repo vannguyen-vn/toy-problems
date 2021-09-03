7c12fc38696e701eccafa93a96829883f30e4e09
Van Nguyen <van.nguyen@texo.design>
2021-09-02 21:07:16 -0700

    Completed: deepEquals

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,42 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange) {
+/* input: 2 objs
+Output: true/fasle
+
+pseudocode
+- create key1 is all keys of obj1
+- create key2 is all keys of obj2
+- if length of key1 !== key2 => false
+- go though the key1
+ - check the value of each key of both to make sure they have children or not
+ - if they have childen and recursion of children is false or they don't have childen && their value is different => false
+return true.
+ */
+var deepEquals = function (apple, orange) {
+  var keys1 = Object.keys(apple);
+  var keys2 = Object.keys(orange);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  if ((typeof apple === 'number' && typeof orange === 'number') && apple !== orange) {
+    return false;
+  }
+
+  for (var key of keys1) {
+    var val1 = apple[key];
+    var val2 = orange[key];
+    var areObjs = isObject(val1) && isObject(val2);
+    if (
+      areObjs && !deepEquals(val1, val2) ||
+      !areObjs && val1 !== val2
+    ) {
+      return false;
+    }
+  }
+  return true;
 };
+
+function isObject(object) {
+  return object != null && typeof object === 'object';
+}
