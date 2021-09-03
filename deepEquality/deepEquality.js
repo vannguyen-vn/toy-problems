fb15e5d138c4ae2c391f3c9ab34c99fdd17256b3
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-02 19:41:21 -0700

    deepEquality complete

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,7 +12,88 @@
   *
   */
 var deepEquals = function(apple, orange) {
+
+  let innerStringify = function(input) {
+    //create an inner function that takes an input object
+
+    //if the input is a number
+    //return a string plus a number
+    if (typeof input === 'number') {
+      return '' += input;
+    }
+
+    //if the input is a string
+      //return the object
+      if (typeof input === 'string') {
+        return input;
+      }
+
+    //if the object is a boolean value
+      //return a string plus the value
+      if (typeof input === 'boolean') {
+        return '' += input;
+      }
+
+    //if the object is null
+      //return a string plus null
+      if (typeof input === 'null') {
+        return 'null';
+      }
+
+      //if the object is an array
+      //create a string
+      // for each value
+        //Add  to the string: call the function with the value
+        //return brackets wrapping the string
+        if (typeof input === 'array') {
+
+          var tempArrString = '';
+
+          input.forEach(element => {
+            tempString += innerStringify(element)
+          })
+
+          return '[' + tempArrString + ']';
+        }
+
+    //if the object is an object
+      //create a string
+      //for each key in the object
+        //add to the string: the key name plus a colon plus a call to the function with the value
+      //return cury braces wrapping a call to the string
+      if (typeof input === 'object') {
+        var tempObjString = ""
+
+        for (var key in input) {
+          tempObjString += '"' + key + '":'  + innerStringify(input[key])
+        }
+
+        return '{' + tempObjString + '}'
+      }
+
+    //close the inner function
+    }
+
+    //set a variable equal to a call to the first object
+    //set a variable equal to a call to the second object
+    var stringifiedApple = innerStringify(apple)
+    var stringifiedOrange = innerStringify(orange)
+
+    console.log(stringifiedApple)
+    console.log(stringifiedOrange)
+
+    //return a comaprison between the two
+    return stringifiedApple === stringifiedOrange
+
 };
+
+
+
+var test1 = deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+console.log('should be true:', test1)
+var test2 = deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+console.log('should be false:', test2)
+
 
 
 /*
@@ -26,6 +107,8 @@ E Empty?
   //make sure the two strings are strictly equal
 
 //Pseudocode
+
+//create an inner function that takes an input object
 
 //if the input is a number
   //return a string plus a number
@@ -51,6 +134,14 @@ E Empty?
     //add to the string: the key name plus a colon plus a call to the function with the value
 
   //return cury braces wrapping a call to the string
+
+//close the inner function
+
+//set a variable equal to a call to the first object
+//set a variable equal to a call to the second object
+
+
+//return a comaprison between the two
 
 
 */
