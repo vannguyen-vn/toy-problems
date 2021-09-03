ed5391f257470acf6e99f9fc09d27495ef477979
Derek Samms <derek.samms@gmail.com>
2021-09-02 19:36:41 -0700

    implemented code for deepEquality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,40 @@
   *
   */
 var deepEquals = function(apple, orange) {
+  //iterate across the object keys
+    //if value is not an object AND value in first array equals the value in the second array
+      //remain true
+    //if both values are objects
+      //repeat recursively
+
+  //OR... could you just stringify both and compare the strings? //Maybe with JSON, but Object.toString doesn't work that way.
+  // if (apple.toString === orange.toString) {
+  //   return true;
+  // }
+  // return false;
+  var appleKeys = Object.keys(apple);
+  var orangeKeys = Object.keys(orange);
+  var areEqual = true;
+
+  if (appleKeys.length !== orangeKeys.length) {
+    return false;
+  }
+  for (var i = 0; i < appleKeys.length; i++) {
+    var currentApple = appleKeys[i];
+    var currentOrange = orangeKeys[i];
+    if (currentApple !== currentOrange) {
+      return false;
+    }
+    if (typeof apple[currentApple] !== typeof orange[currentOrange]) {
+      return false;
+    }
+    if (typeof apple[currentApple] !== 'object' && apple[appleKeys[i]] !== orange[orangeKeys[i]]) {
+      return false;
+    }
+    areEqual = deepEquals(apple[currentApple], orange[currentOrange]);
+
+  }
+
+  return areEqual;
+
 };
