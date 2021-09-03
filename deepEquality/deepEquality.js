dd4a6cb7d2c0898cea2932ca207d3942d297bb7f
Ryan Rhoads <rhoads.ryan.grant@gmail.com>
2021-09-02 20:01:22 -0700

    Attempting Refactor for 16/16

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -10,5 +10,27 @@
   *
   */
  var deepEquals = function(apple, orange) {
-  return JSON.stringify(apple) === JSON.stringify(orange) ? true : false;
+  //debugger;
+  // return JSON.stringify(apple) === JSON.stringify(orange) ? result = true : result = false; // One line 15/16
+  let result = true;
+
+  // Store keys
+  let aKeys = Object.keys(apple);
+  let oKeys = Object.keys(orange);
+
+  // If keys don't match, set result to false
+  if (aKeys.length !== oKeys.length) {
+    return false;
+  }
+
+  for (var i = 0; i < aKeys.length; i++) {
+    let key = aKeys[i];
+    if (JSON.stringify(apple[key]) !== JSON.stringify(orange[key])) {
+      return false;
+    }
+  }
+
+  return true;
 };
+
+// console.log(deepEquals( { a:1 , b:[1,3] } , { b:[1,3], a:1 } ));
