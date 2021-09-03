384eaa7179bc8f21d65ce7e59d0029b44b90f462
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-02 19:14:22 -0700

    Drafted plan + logic + pseudocode

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -13,3 +13,44 @@
   */
 var deepEquals = function(apple, orange) {
 };
+
+
+/*
+
+I two objects
+O Boolean value
+C No cyclical object structures
+E Empty?
+
+//Process - Copy each object into its own string
+  //make sure the two strings are strictly equal
+
+//Pseudocode
+
+//if the input is a number
+  //return a string plus a number
+
+//if the input is a string
+  //return the object
+
+//if the object is a boolean value
+  //return a string plus the value
+
+//if the object is null
+  //return a string plus null
+
+//if the object is an array
+  // for each value
+    //Add  to the string: call the function with the value
+
+//return brackets wrapping the string
+
+//if the object is an object
+  //create a string
+  //for each key in the object
+    //add to the string: the key name plus a colon plus a call to the function with the value
+
+  //return cury braces wrapping a call to the string
+
+
+*/
