fe3ecf039e3e437b89855e8c56d6f9cef0c5fa74
Bishal Gautam <bgautam.se@gmail.com>
2021-09-02 19:45:19 -0700

    deep equals

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,29 @@
   *
   */
 var deepEquals = function(apple, orange) {
+  //must do === for each descendant of each key etc.
+  if (apple === orange) return true;
+  if (typeof(apple) !== 'object' || typeof(orange) !== 'object') return false;
+  if (apple === null || orange === null) return false;
+  if (Object.keys(apple).length !== Object.keys(orange).length) return false;
+
+  // var isdeepEqual = true;
+
+  for (var key in apple) {
+    if (!deepEquals(apple[key], orange[key])) {
+      return false;
+    } else {
+      if (!Object.keys(orange).includes(key)) {
+        return false;
+      }
+    } else {
+      if ((typeof(orange[key]) === 'function' || typeof(apple[key]) === 'function')) {
+        if (orange[key].toString() !== apple[key].toString()) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+
 };
