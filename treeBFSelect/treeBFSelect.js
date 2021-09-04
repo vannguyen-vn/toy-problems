c79a964a9e281c36ca977de3d382e3d404d8c2e0
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-03 20:00:30 -0700

    Time limit submission

--- treeBFSelect/treeBFSelect.js
+++ treeBFSelect/treeBFSelect.js
@@ -39,6 +39,25 @@ var Tree = function(value) {
 
 Tree.prototype.BFSelect = function(filter) {
   // return an array of values for which the function filter(value, depth) returns true
+
+  var passingNodes = [];
+
+  var innerHelper = function (node) {
+
+    if (filter(node.value)){
+      passingNodes.push(node.value)
+    }
+
+    if(node.children.length > 0) {
+      node.children.forEach(child => node.BFSelect.call(child, filter))
+    }
+
+  }
+
+  innerHelper(this);
+
+  return passingNodes
+
 };
 
 /**
@@ -94,3 +113,21 @@ Tree.prototype.removeChild = function(child) {
     throw new Error('That node is not an immediate child of this tree');
   }
 };
+
+
+var root1 = new Tree(1);
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7);
+console.log('[1, 3, 5, 7]', root1.BFSelect(function (value, depth) {
+  return value % 2;
+}))
+// [1, 3, 5, 7]
+
+console.log('[2, 3]', root1.BFSelect(function (value, depth) {
+  return depth === 1;
+}))
+// [2, 3]
