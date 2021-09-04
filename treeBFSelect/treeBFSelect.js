03151f7cdf3586fc4fbdcbe7a21a530e33b9dcfb
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-03 20:10:18 -0700

    treeBFSelect attempted

--- treeBFSelect/treeBFSelect.js
+++ treeBFSelect/treeBFSelect.js
@@ -30,15 +30,39 @@
  * Basic tree that stores a value.
  */
 
-var Tree = function(value) {
+var Tree = function (value) {
   this.value = value;
   this.children = [];
 };
 
 
+// input: filter
+// output: array of nodes that pass the filters test
+// constraints: call each node in breadth first order
+// edge case: if no nodes meet criteria, return empty array ?
+Tree.prototype.BFSelect = function (filter, depth) {
+  depth = depth || 0
+  // base case: if no children
+  // return that node run through the filter
+  var filteredRoot;
+  // run root through filter
+  if (filter(this.value, depth)) {
+    filteredRoot = [this.value]
+  } else {
+    filteredRoot = []
+  }
 
-Tree.prototype.BFSelect = function(filter) {
+  // map or loop through children nodes
+
+  for (var i = 0; i < this.children.length; i++) {
+    var child = this.children[i]
+    // for each
+    // call filter on it
+    filteredRoot = filteredRoot.concat(child.BFSelect(filter, depth + 1))
+  }
+
   // return an array of values for which the function filter(value, depth) returns true
+  return filteredRoot
 };
 
 /**
@@ -49,7 +73,7 @@ Tree.prototype.BFSelect = function(filter) {
   * add an immediate child
   * (wrap values in Tree nodes if they're not already)
   */
-Tree.prototype.addChild = function(child) {
+Tree.prototype.addChild = function (child) {
   if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
   }
@@ -67,7 +91,7 @@ Tree.prototype.addChild = function(child) {
   * check to see if the provided tree is already a child of this
   * tree __or any of its sub trees__
   */
-Tree.prototype.isDescendant = function(child) {
+Tree.prototype.isDescendant = function (child) {
   if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
@@ -85,7 +109,7 @@ Tree.prototype.isDescendant = function(child) {
 /**
   * remove an immediate child
   */
-Tree.prototype.removeChild = function(child) {
+Tree.prototype.removeChild = function (child) {
   var index = this.children.indexOf(child);
   if (index !== -1) {
     // remove the child
@@ -94,3 +118,23 @@ Tree.prototype.removeChild = function(child) {
     throw new Error('That node is not an immediate child of this tree');
   }
 };
+
+
+//Example:
+var root1 = new Tree(1);
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7);
+console.log(root1.BFSelect(function (value, depth) {
+  return value % 2;
+}))
+// [1, 3, 5, 7]
+
+console.log(root1.BFSelect(function (value, depth) {
+  return depth === 1;
+}))
+// [2, 3]
+
