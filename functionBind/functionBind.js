76e9cd9f92c5a14354d3710f8f4bb71d6d337660
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-08 20:13:16 -0700

    Used apply to bind insdie a fn

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -23,10 +23,30 @@
  *
 */
 
-var bind = function(
-) {
-  // TODO: Your code here
+// input: function, object
+// output: function that binds the original fn and the object or info
+// constraints: ?
+// edge cases,
+var bind = function(fn, obj) {
+  // make something refer to something specific
+  // call the fn using the object as a parameter?
+  return function () {
+    fn.apply(obj)
+  }
 };
+
+// example 1:
+
+ var alice = {
+   name: 'alice',
+   shout: function(){
+     alert(this.name);
+   }
+ }
+ var boundShout = bind(alice.shout, alice);
+ boundShout(); // alerts 'alice'
+ boundShout = bind(alice.shout, {name: 'bob'});
+ boundShout(); // alerts 'bob'
 
 /*
  * Function.prototype.bind:
