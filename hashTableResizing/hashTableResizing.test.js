52b74773212e791ba7ce5293a337b4322fe5a9ee
Justin Beere <beere.justin@gmail.com>
2021-09-09 19:55:52 -0700

    end of alotted time

--- hashTableResizing/hashTableResizing.test.js
+++ hashTableResizing/hashTableResizing.test.js
@@ -1,7 +1,7 @@
 const makeHashTable = require('./hashTableResizing.js');
 const getIndexBelowMaxForKey = require('./hashTableResizing.js')
 
-describe.only("hashTableResizing Tests", function(){
+describe.skip("hashTableResizing Tests", function(){
   var testTable = makeHashTable();
 
   it('Should have methods named "insert", "remove" and "retrieve"', function(){
@@ -13,7 +13,8 @@ describe.only("hashTableResizing Tests", function(){
 
   it('Should store values that are retrievable'), function() {
 
-  });
+  }
+});
 
   // it("Should return the first number that occurred an even number of times", function(){
   //   expect(evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4])).toBe(4);
@@ -23,4 +24,4 @@ describe.only("hashTableResizing Tests", function(){
   //   expect(evenOccurrence([1, 7, 2, 4, 5, 8, 9, 6])).toBe(null);
   //   expect(evenOccurrence([1, 4, 7, 4, 2, 4, 5, 6, 8, 6, 9, 4, 6, 4])).toBe(null)
   // });
-});
+// });
