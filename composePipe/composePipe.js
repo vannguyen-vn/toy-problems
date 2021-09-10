63c55a86b86655ce21047ad3ab6a957f78d547f3
Joseph Moon <joemoondev@gmail.com>
2021-09-09 19:53:31 -0700

    Implement pipe() using reduce

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -1,7 +1,7 @@
 /*
  * Write Compose and Pipe functions.
- * 
- * Step 1: Implement the function Compose: 
+ *
+ * Step 1: Implement the function Compose:
  *
  * Compose should return a function that is the composition of a list of
  * functions of arbitrary length.
@@ -19,11 +19,11 @@
  * Step 2: Implement the function Pipe:
  *
  * Pipe composes a series of functions and returns the resulting function.
- * 
+ *
  * Each function is called on the return value of the preceding function.
  *
  * You can view pipe as moving left to right through its arguments.
- * 
+ *
  * Pipe Example:
  *  var add2 = function(number){ return number + 2; }
  *  var multiplyBy3 = function(number){ return number * 3; }
@@ -37,4 +37,10 @@ var compose = function() {
 };
 
 var pipe = function() {
+  var funcs = [...arguments];
+  return function(i) {
+    return funcs.reduce((memo, x) => {
+      return x(memo);
+    }, i);
+  }
 };
