471bf08334c12adc2afc850fe0bca20ef791109e
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-10 20:15:25 -0700

    return string of letters in common between two strings

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -11,7 +11,34 @@
  */
 
 
-
+// input: 2 strings
+// output: string of common letters
+// constraints: look for letters in order of appearance, skip spaces, no duplicates
+// edge cases: N/A
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  // create obj
+  var inCommon = ''
+  var letters = {}
+  // loop through string1
+  for (var i = 0; i < string1.length; i++) {
+    // if letter doesn't exist in obj
+    if (letters[string1[i]] === undefined) {
+      // add it as a key with value false (for more than two strings could use an array)
+      letters[string1[i]] = false
+    }
+  }
+
+  // loop through string2
+  for (var j = 0; j < string2.length; j++) {
+     // if letter exists as a key
+     if (letters[string2[j]] !== undefined) { // if more strings, could push true or false to the array for that letter depending on if it has the letter
+       // add it to the return str
+       inCommon += string2[j]
+     }
+  }
+   return inCommon
 };
+
+//Example:
+console.log( commonCharacters('acexivou', 'aegihobu') )
+// Returns: 'aeiou'
