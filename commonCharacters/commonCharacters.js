7ec02b8daec68c8969905099d56f33c9df900e11
Justin Beere <beere.justin@gmail.com>
2021-09-10 19:26:42 -0700

    first attempt

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,23 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  // using string1 iterate through each character and check if it exists in string2
+  let results = {};
+  let commonString = '';
+
+  for (var i = 0; i < string1; i++) {
+    for (var j = 0; j < string2; j++) {
+      if (string1[i] === string2[j] && results[string1[i]] === undefined) {
+        console.log(string1[i]);
+        results[string1[i]] = true;
+      }
+    }
+  }
+  for (var char in results) {
+    commonString = commonString + char;
+  }
+  return commonString;
 };
+
+
+console.log(commonCharacters('acexivou', 'aegihobu'));
