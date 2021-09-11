431bb7e19b0d15bd2873f786e9d28db6a282d21f
Alex Romero <alexromeroreyes09@gmail.com>
2021-09-10 20:31:46 -0700

    commonCharacters Toy Problem Submission #2

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -13,5 +13,35 @@
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  // Initialize a result string
+  var result = '';
+  // Initialize a temp string
+  var temp = '';
+  // Create object for string1
+  var obj = {};
+  // Iterate over the string1
+  for (var i = 0; i < string1.length; i++) {
+    // For each letter place in an object
+    if (string1[i] !== obj[string1[i]]) {
+      obj[string1[i]] = 1;
+    } else {
+      obj[string1[i]] += 1;
+    }
+  }
+  // Iterate over string2
+  for (var j = 0; j < string2.length; j++) {
+    if (obj.hasOwnProperty(string2[j])) {
+      // If it is, push into temp string
+      temp += string2[j];
+    }
+    // Iterate over temp
+  }
+  for (var k = 0; k < temp.length; k++) {
+    // Check that index of matches index of string1
+    if (temp.indexOf(k) === string1.indexOf(k)){
+      // If so, push into result
+      result += temp[k];
+    }
+  }
+  return result;
 };
