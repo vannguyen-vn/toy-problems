27b41077878b1234f04f25b211ed9a4528819509
Daniel Prejs <danielnprejs@gmail.com>
2021-09-12 11:17:39 -0700

    commonCharacters attempt 2

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -13,31 +13,57 @@
 
 
 var commonCharacters = function (string1, string2) {
-  var commonLetters = {};
-  var stringObj = {}
-  for (var i = 0; i < string1.length; i++) {
-    if (string1[i] !== ' ') {
-      stringObj[string1[i]] = true;
-    }
-  }
-  var checkString = (string) => {
-    for (var i = 0; i < string.length; i++) {
-      if (string[i] !== ' ') {
-        if (stringObj[string[i]]) {
-          commonLetters[string[i]] = true;
-        }
-      }
-    }
-  }
+  // Solution that retains order letters appear in
+  var commonLetters = new Set(string1);
+  commonLetters.delete(' ');
   for (var i = 1; i < arguments.length; i++) {
-    checkString(arguments[i]);
-    stringObj = commonLetters;
-    commonLetters = {};
+    commonLetters.forEach((letter) => {
+      var reg = new RegExp(`${letter}`, 'i');
+      if (!reg.test(arguments[i])) {
+        commonLetters.delete(letter);
+      }
+    })
   }
+  commonLetters = [...commonLetters].join('');
+  return commonLetters;
 
+  //regex solution
 
-  return Object.keys(stringObj).join('')
-  // TODO: Your code here!
+  // var reg = new RegExp(`[${string1}]`, 'g');
+  // var commonLetters = '';
+  // for(var i = 1; i < arguments.length; i++) {
+  //   commonLetters = arguments[i].match(reg);
+  //   commonLetters = commonLetters.filter(letter => letter !== ' ');
+  //   commonLetters = [...new Set(commonLetters)];
+  //   commonLetters = commonLetters.join('');
+  //   reg = new RegExp(`[${commonLetters}]`, 'g');
+  // }
+  // return commonLetters;
+
+  // Time optimize solution
+
+  // var commonLetters = {};
+  // var stringObj = {};
+  // for (var i = 0; i < string1.length; i++) {
+  //   if (string1[i] !== ' ') {
+  //     stringObj[string1[i]] = true;
+  //   }
+  // }
+  // var checkString = (string) => {
+  //   for (var i = 0; i < string.length; i++) {
+  //     if (string[i] !== ' ') {
+  //       if (stringObj[string[i]]) {
+  //         commonLetters[string[i]] = true;
+  //       }
+  //     }
+  //   }
+  // }
+  // for (var i = 1; i < arguments.length; i++) {
+  //   checkString(arguments[i]);
+  //   stringObj = commonLetters;
+  //   commonLetters = {};
+  // }
+  // return Object.keys(stringObj).join('');
 };
 
 // module.exports = commonCharacters;
