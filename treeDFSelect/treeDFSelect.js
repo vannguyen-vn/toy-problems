b12be169d7686e7729eaa42e5793927f94c42639
Cora Durham <cora5du@gmail.com>
2021-09-20 07:13:50 -0500

    removed test case from treeDFSelect

--- treeDFSelect/treeDFSelect.js
+++ treeDFSelect/treeDFSelect.js
@@ -106,25 +106,3 @@ Tree.prototype.removeChild = function(child) {
     throw new Error('That node is not an immediate child of this tree');
   }
 };
-
-
-
-var root1 = new Tree(1);
-var branch2 = root1.addChild(2);
-var branch3 = root1.addChild(3);
-var leaf4 = branch2.addChild(4);
-var leaf5 = branch2.addChild(5);
-var leaf6 = branch3.addChild(6);
-var leaf7 = branch3.addChild(7);
-
-console.log(root1);
-
-console.log(root1.DFSelect(function (value, depth) {
-  return value % 2;
-}));
-// [1, 5, 3, 7]
-
-console.log(root1.DFSelect(function (value, depth) {
-  return depth === 1;
-}));
-// [2, 3]
