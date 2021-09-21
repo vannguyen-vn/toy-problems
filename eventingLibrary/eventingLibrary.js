67a862e9a96630c6bb2a90d8160d19c764fdbbf7
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-20 19:52:56 -0700

    eventingLibrary attempted

--- eventingLibrary/eventingLibrary.js
+++ eventingLibrary/eventingLibrary.js
@@ -20,7 +20,29 @@
  * - It is not necessary to write a way to remove listeners.
  */
 
-var mixEvents = function(obj) {
-  // TODO: Your code here
+// input: obj
+// output: object now extended with 2 new properties?
+// an on function (that takes 2 parameters- eventName, callback)
+// a function that triggers the on function
+// constraints: N/A
+// edge cases:
+var mixEvents = function (obj) {
+  // add on property (value is the function)
+  obj.on = function(event, callback) {
+    console.log('event')
+  }
+  // add trigger property
+  obj.trigger = function(desiredEvent) {
+    obj.on(desiredEvent);
+  }
+
   return obj;
 };
+
+// Example usage:
+var obj = mixEvents({ name: 'Alice', age: 30 });
+obj.on('ageChange', function () { // On takes an event name and a callback function
+  console.log('Age changed');
+});
+obj.age++;
+console.log(obj.trigger('ageChange')); // This should call our callback! Should log 'age changed'.
