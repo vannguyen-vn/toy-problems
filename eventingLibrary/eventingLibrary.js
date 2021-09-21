825cc45d8c8136075a46c57831022676126ecc91
Andrew Cho <andrewgunncho@gmail.com>
2021-09-20 21:49:03 -0700

    Rearranged order of functions for eventingLibrary

--- eventingLibrary/eventingLibrary.js
+++ eventingLibrary/eventingLibrary.js
@@ -24,20 +24,18 @@ var mixEvents = function (obj) {
 
   var events = {};
 
-  obj.on = (event, callback) => {
-
-    events[event] = events[event] || [];
-    events[event].push(callback);
-  };
-
   obj.trigger = (event) => {
-
     if (events[event]) {
       var args = Array.prototype.slice.call(arguments, 1);
       events[event].forEach((callback) => {
         callback.apply(obj, args);
       })
     }
+  };
+
+  obj.on = (event, callback) => {
+    events[event] = events[event] || [];
+    events[event].push(callback);
   };
 
   return obj;
