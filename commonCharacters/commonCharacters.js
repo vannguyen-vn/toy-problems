7480e80aac9976edc2e469f254536e448be9eea2
huongtran1993 <huongtran1993@gmail.com>
2021-09-24 20:11:36 -0700

    completed commonCharacters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -13,5 +13,22 @@
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  args = Array.prototype.slice.call(arguments);
+  var mainStr = args[0];
+  var strArr = args.slice(1);
+  var result = '';
+  var isCommonChar = function(char) {
+    for (var i = 0; i < strArr.length; i++) {
+      if (strArr[i].indexOf(char) === -1) {
+        return false;
+      }
+    }
+    return true;
+  }
+  for (var i = 0; i < mainStr.length; i++) {
+    if (isCommonChar(mainStr[i])) {
+      result += mainStr[i];
+    }
+  }
+  return result;
 };
