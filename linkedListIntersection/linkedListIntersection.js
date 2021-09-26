0591b15490fd07d6cc163cdb67a8d6269d957711
Van Nguyen <van.nguyen@texo.design>
2021-09-26 16:38:39 -0700

    Completed LinkedListIntersection

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -35,27 +35,38 @@ function Node (val) {
   return obj;
 }
 
-
-
 function linkedListIntersection(list1, list2) {
-  if(list1 === null && list2 === null){
+  if(list1 === null || list2 === null){
     return null
   }
   let pointer1 = list1;
-  let pointer2 = list2
-  while(pointer1 != pointer2) {
-    pointer1= pointer1.next;
+  let pointer2 = list2;
+  while(pointer1 !== pointer2) {
+    pointer1 = pointer1.next;
     pointer2= pointer2.next;
     if(pointer1 === pointer2) {
       return pointer1
     }
-    if(pointer1=== null){
+    if(pointer1 === null){
       pointer1 = list2;
     }
-    if(pointer2===nulll) {
+    if(pointer2 === null) {
       pointer2 = list1;
     }
   }
+
   return pointer1;
 }
+
+// var list1 = Node('A');
+//     var nodeB = list1.next = Node('B');
+//     var nodeC = nodeB.next = Node('C');
+//     var nodeD = nodeC.next = Node('D');
+//     var nodeE = nodeD.next = Node('E');
+//     var nodeF = nodeE.next = Node('F');
+//     var list2 = Node('X');
+//     var nodeY = list2.next = Node('Y');
+//     nodeY.next = nodeD;
+
+//     console.log(linkedListIntersection(list1, list2));
 
