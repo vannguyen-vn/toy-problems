40942c5def1e73c7a8aba873231bd204ce2fb06d
Nan Jiang <dora.nan.j@gmail.com>
2021-09-27 23:48:40 -0700

    attempted linked list intersection;

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -1,30 +1,32 @@
+//attempted: Sep 27
+
 /*
 *
 * Linked List Intersection
 *
-* Write a function linkedListIntersection that returns the node 
-* at which the intersection of two linked lists begins, 
+* Write a function linkedListIntersection that returns the node
+* at which the intersection of two linked lists begins,
 * or null if there is no such intersection.
-* 
+*
 * Example:
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return D 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return D
 * as the node of intersection.
-* 
+*
 *    A → B → C
 *             ↘
 *               D → E → F
 *             ↗
 *        X → Y
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return NULL 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return NULL
 * as there is no point of intersection.
-* 
+*
 *    A → B → C → D
 *    X → Y → Z
-* 
+*
 */
 
 // Helper function (do not edit)
@@ -39,5 +41,40 @@ function Node (val) {
 
 function linkedListIntersection(list1, list2) {
   // TODO: Implement this function!
-}
 
+  // convert list2 to an object
+  var objList = {};
+  var convertToObject = function(list) {
+    objList[list.value] = list.value;
+    if (list.next === null) {
+      return;
+    } else {
+      convertToObject(list.next);
+    }
+  }
+
+  convertToObject(list2);
+
+  var findIntersect = function(list) {
+    if (objList.hasOwnProperty(list.value)) {
+      console.log(`1: ${JSON.stringify(Node(list.value))}`);
+      return Node(list.value);
+    } else if ( list.next === null ) {
+      console.log(`2: ${list.value}`);
+      return null;
+    } else {
+      console.log(`3: ${list.value} ${list.next}`);
+      return findIntersect(list.next);
+    }
+  }
+
+  return findIntersect(list1);
+
+}
+/*
+const ls1 = {value: 'A', next: {value: 'B', next: {value: 'C', next: {value: 'D', next: {value: 'E', next:{value: 'F', next: null}}}}}};
+const ls2 = {value: 'X', next: {value: 'Y', next: {value: 'D', next: {value: 'E', next: {value: 'F', next: null}}}}};
+console.log(`ls1 & ls2: ${linkedListIntersection(ls1, ls2)}`);
+
+
+*/
