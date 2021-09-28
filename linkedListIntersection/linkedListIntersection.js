dd630400b08b4f4567514ffa915f3063420f3a76
Bishal Gautam <bgautam.se@gmail.com>
2021-09-27 19:35:05 -0700

    linkedlist Intersection, needed conditional inside while loop to return pointer before it was checked for being null

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -38,18 +38,21 @@ function Node (val) {
 
 
 function linkedListIntersection(list1, list2) {
-  if (list1.value === null || list2.value === null) return null;
+  if (list1 === null || list2 === null) return null;
   //traverse the linked list until
   //while pointers are not equal to each other, if  pointer1s value is null (!value), then pointer1 points at
   //if either reaches null, return null
   let pointer1 = list1;
   let pointer2 = list2;
   //if pointer1 and pointer2 are the same value, could be a value or they could both null where they would also be equal, so would return null or the value
-  while (pointer1.value !== pointer2.value) {
+  while (pointer1 !== pointer2) {
    pointer1 = pointer1.next;
    pointer2 = pointer2.next;
    //changes position one by one until pointer1 === pointer 2
    //if a pointer reaches null, make it point to the other lists head
+   if (pointer1 === pointer2) {
+     return pointer 1;
+   }
    if (pointer1 === null) {
      pointer1 = list2;
    }
