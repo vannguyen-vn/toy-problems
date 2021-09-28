cf73c935fd5b3a0de393b7ced82ed356fb573a3c
Nan Jiang <dora.nan.j@gmail.com>
2021-09-27 22:47:44 -0700

    submit romanNumeralTranslator

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -1,4 +1,4 @@
-
+//Attempted Sep 27
 /**
  * Given a roman numeral as input, write a function that converts the roman
  * numeral to a number and outputs it.
@@ -29,5 +29,35 @@ var DIGIT_VALUES = {
 
 var translateRomanNumeral = function(romanNumeral) {
 // TODO: Implement me!
+  if (romanNumeral.length === 0 || romanNumeral.length === undefined) { return null; }
 
+  let sum = 0;
+  for (var i = 0; i < romanNumeral.length; i++)   {
+    if (!DIGIT_VALUES.hasOwnProperty(romanNumeral[i])) {
+      return null;
+    } else if (
+      (romanNumeral[i] === 'I' || romanNumeral[i] === 'C' || romanNumeral[i] === 'X') &&
+      (DIGIT_VALUES[romanNumeral[i]] < DIGIT_VALUES[romanNumeral[i+1]])
+      ) {
+        sum -= DIGIT_VALUES[romanNumeral[i]];
+      } else {
+        sum += DIGIT_VALUES[romanNumeral[i]];
+      }
+  }
+
+  return sum;
 };
+
+/*
+
+const s1 = 'III';
+const s2 = 'IV';
+const s3 = 'LX';
+const s4 = 'MCMXCIV';
+const s5 = 'MCMXCIA'
+console.log(`s1: ${translateRomanNumeral(s1)}`); // 3
+console.log(`s1: ${translateRomanNumeral(s2)}`); // 4
+console.log(`s1: ${translateRomanNumeral(s3)}`); // 60
+console.log(`s1: ${translateRomanNumeral(s4)}`); // 1994
+console.log(`s1: ${translateRomanNumeral(s5)}`); // null
+*/
