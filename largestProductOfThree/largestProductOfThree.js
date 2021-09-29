b404c2e598c20f8b1d22b4c7b8177127b792a22d
Nan Jiang <dora.nan.j@gmail.com>
2021-09-28 22:58:55 -0700

    attempted largest product of three

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -10,4 +10,18 @@
 
 var largestProductOfThree = function(array) {
   // TODO: everything
+  const dfs = function (array, start, current_value, depth) {
+    if (depth === 3) {
+      return current_value;
+    }
+    if (start >= array.length) {
+      return -1;
+    }
+    var curMax = Number.NEGATIVE_INFINITY;
+    for (var i=start; i<array.length; i++) {
+      curMax = Math.max(curMax, dfs(array, i+1, current_value*array[i], depth+1));
+    }
+    return curMax;
+  };
+  return dfs(array, 0, 1, 0);
 };
