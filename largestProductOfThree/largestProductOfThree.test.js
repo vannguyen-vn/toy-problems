7c69116fe7550efc6a8d844307ed722084161dfa
Daniel Prejs <danielnprejs@gmail.com>
2021-09-28 20:02:56 -0700

    largest product of three attempt 1

--- largestProductOfThree/largestProductOfThree.test.js
+++ largestProductOfThree/largestProductOfThree.test.js
@@ -4,4 +4,7 @@ describe('largest product of three', () => {
   test('it should find the largest product', () => {
     expect(largestProductOfThree([2, 1, 3, 7])).toEqual(42);
   })
+  test('it can handle negative numbers', () => {
+    expect(largestProductOfThree([-2, 1, -3, 7])).toEqual(42);
+  })
 })
