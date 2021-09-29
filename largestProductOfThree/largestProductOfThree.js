60bb70d105b7bb1c49a1420f19744c58aa02eb28
Derek Samms <derek.samms@gmail.com>
2021-09-28 20:50:46 -0700

    attempted largestProductOfThree

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -9,5 +9,31 @@
 
 
 var largestProductOfThree = function(array) {
-  // TODO: everything
+  //find the largest three numbers
+    //find one largest
+    //search splice to find next largest
+    //search new splice to find third largest
+  //multiply them together
+
+  var firstLargest, secondLargest, thirdLargest;
+  var product = 1;
+
+  var findLargest = function(array) {
+    var largest = array[0];
+    for (var i = 1; i < array.length; i++) {
+      if (array[i] > largest) {
+        largest = array[i];
+      }
+    }
+    return largest;
+  }
+
+  firstLargest = findLargest(array);
+  array.splice(array.indexOf(firstLargest), 1);
+  secondLargest = findLargest(array);
+  array.splice(array.indexOf(secondLargest), 1);
+  thirdLargest = findLargest(array);
+
+  return firstLargest * secondLargest * thirdLargest;
+
 };
