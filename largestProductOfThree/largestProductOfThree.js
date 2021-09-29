95408bff0778411505f0154245642c3030407bfe
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-28 19:24:31 -0700

    largest product returned using sort and returning the largest three numbers multiplied

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,7 +7,14 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-
+// input array of numbers
+// output number (largest product of three numbers from input array)
+// constraint: N/A
+// edge cases: empty array, less than 3?
 var largestProductOfThree = function(array) {
-  // TODO: everything
+   array.sort()
+   return array[array.length - 1] * array[array.length - 2] * array[array.length - 3]
 };
+
+largestProductOfThree([2, 1, 3, 7]) === 42
+largestProductOfThree([-2, -1, -3, -7]) === - 42
