c61c74aa26de64921541716e2b6c831abac6af3c
huongtran1993 <huongtran1993@gmail.com>
2021-09-29 21:00:06 -0700

    completed linkedListIntersection

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -2,29 +2,29 @@
 *
 * Linked List Intersection
 *
-* Write a function linkedListIntersection that returns the node 
-* at which the intersection of two linked lists begins, 
+* Write a function linkedListIntersection that returns the node
+* at which the intersection of two linked lists begins,
 * or null if there is no such intersection.
-* 
+*
 * Example:
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return D 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return D
 * as the node of intersection.
-* 
+*
 *    A → B → C
 *             ↘
 *               D → E → F
 *             ↗
 *        X → Y
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return NULL 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return NULL
 * as there is no point of intersection.
-* 
+*
 *    A → B → C → D
 *    X → Y → Z
-* 
+*
 */
 
 // Helper function (do not edit)
@@ -38,6 +38,20 @@ function Node (val) {
 
 
 function linkedListIntersection(list1, list2) {
-  // TODO: Implement this function!
+  var currNode1 = list1;
+  var currNode2 = list2;
+  while (!(currNode2 === null && currNode1 === null)) {
+    if (currNode2 === null) {
+      if (currNode1.next !== null) {
+        currNode2 = list2;
+      }
+      currNode1 = currNode1.next;
+    } else if (currNode2 !== null && currNode1.value !== currNode2.value) {
+      currNode2 = currNode2.next;
+    } else {
+      return currNode1
+    }
+  }
+  return null;
 }
 
