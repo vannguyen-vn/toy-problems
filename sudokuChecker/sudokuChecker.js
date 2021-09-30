fddcefd1a53c184c638db252fb8173c719e8a4f8
Ryan Rhoads <rhoads.ryan.grant@gmail.com>
2021-09-29 20:12:39 -0700

    Implemented Rows Check

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -3,10 +3,10 @@ Given a full sudoku board, return 'solved' if the board is solved, or 'invalid' 
 
 A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9.
 
-Input: A String representing the board. 
+Input: A String representing the board.
 Output: 'solved' if the board is valid, 'invalid' if it isn't
 
-Example input: 
+Example input:
 "735814296\n
 896275314\n
 214963857\n
@@ -18,9 +18,28 @@ Example input:
 157638429"
 */
 
-
-
 function sudokuChecker(board) {
-  // Your code here.
-  
+  const newBoard = board.split('\n');
+  console.log(newBoard);
+
+  const checkDuplicates = (string) => {
+    for (int i = 0; i < string.length; i++) {
+      for (int j = 0; j < string.length; j++) {
+        if (string[i] === string[j]) return true;
+      }
+    }
+  }
+
+  //check rows
+  for(let i = 0; i < newBoard.length; i++) {
+    if (checkDuplicates(bewBoard[i])) return true;
+  }
+  //check cols
+
+  //check grid
+
+  return false;
 }
+
+let board1 = "735814296\n896275314\n214963857\n589427163\n362189745\n471356982\n923541678\n648792531\n157638429";
+sudokuChecker(board1);
