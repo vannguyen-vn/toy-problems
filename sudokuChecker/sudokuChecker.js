984066d7a1503d978aedbc93b6732a575a17c37b
Aaron Tran <altran32@gmail.com>
2021-09-29 21:11:59 -0700

    pseudo

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -3,10 +3,10 @@ Given a full sudoku board, return 'solved' if the board is solved, or 'invalid' 
 
 A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9.
 
-Input: A String representing the board. 
+Input: A String representing the board.
 Output: 'solved' if the board is valid, 'invalid' if it isn't
 
-Example input: 
+Example input:
 "735814296\n
 896275314\n
 214963857\n
@@ -19,8 +19,13 @@ Example input:
 */
 
 
-
+let board = '735814296\n896275314\n214963857\n589427163\n362189745\n471356982\n923541678\n648792531\n157638429'
 function sudokuChecker(board) {
   // Your code here.
-  
+  // organize board into an array of arrays of integers
+  // validate rows
+  // validate columns
+  // validate grid
+  // return 'solved' if conditions have been met
+  // return 'invalid' if not
 }
