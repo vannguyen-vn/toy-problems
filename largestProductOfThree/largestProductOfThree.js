ae572b27439b0a3ed3492251af54876543142d56
huongtran1993 <huongtran1993@gmail.com>
2021-09-29 22:12:04 -0700

    completed largestProductOfThree

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -9,5 +9,22 @@
 
 
 var largestProductOfThree = function(array) {
-  // TODO: everything
+  var input = array;
+  if (input.length < 3) {
+    return 0;
+  } else if (input.length === 3) {
+    return input[0] * input[1] * input[2];
+  }
+  input.sort(function (a, b) {
+    return b - a;
+  });
+  var productOfLastTwo = input[input.length - 1] * input[input.length - 2];
+  var productOfSecondThird = input[1] * input[2];
+  if (input[0] <= 0) {
+    return input[0] * input[1] * input[2];
+  } else if (input[0] > 0 && productOfLastTwo > productOfSecondThird) {
+    return input[0] * productOfLastTwo;
+  } else {
+    return input[0] * productOfSecondThird;
+  }
 };
