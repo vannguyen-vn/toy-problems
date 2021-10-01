f1c15831680bcd2f60afcc68a37e9b3721749ca8
Matthew Boyle <matthewtboyle89@gmail.com>
2021-09-30 19:38:13 -0700

    intergerReverse 1

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,27 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  var result = '';
+  var innerFunc = (num) => {
+    var digit = num % 10;
+    result += digit;
+    var nextNum = Math.floor(num / 10);
+    if (nextNum === 0) {
+      return
+    } else {
+      innerFunc(nextNum)
+    }
+  }
+
+  innerFunc(number)
+  return Number(result);
 }
