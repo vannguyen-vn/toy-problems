ddc8167cf16cb436a92b0951793cf06902d83d5c
Bishal Gautam <bgautam.se@gmail.com>
2021-09-30 20:23:01 -0700

    rev int pos

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,19 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  var rev = 0;
+  while(number > 0) {
+    rev = (rev * 10) + (number % 10);
+    number = parseInt(number/10);
+  }
+  return rev;
 }
