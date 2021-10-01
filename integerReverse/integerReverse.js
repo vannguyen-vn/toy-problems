64ed83907d28604f41a66223a49e22189a7a3756
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-09-30 19:55:41 -0700

    First Attempt

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,37 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  var negative = false;
+
+  if (number < 0) {
+    number = -number;
+    var negative = true;
+  }
+  // Create Variable to represent our reversed number
+  var reverse = 0;
+
+  while (number > 0) {
+    // getting the remainder of number modulo 10 will give us the last digit
+    var lastDigit = number % 10;
+    // we cant concat digits so multiplying reverse by 10 will open a slot to add the next last digit,
+    // if reverse is greater than 0. If not then reverse becomes the last digit.
+    // 0 * 10 = 0, 0 + 4 = 4 / 4 * 10 = 40, / 40 + 5 = 45. this is the example if input is 54.
+    reverse = (reverse * 10) + lastDigit;
+    // this step allows us to 'pop' off the last digit.
+    // ex: 318 / 10 = 31.8 but Math.floor makes sure that 31 is outputted.
+    number = Math.floor(number / 10);
+  }
+
+  return (negative ? -reverse : reverse);
 }
+
+module.exports = reverseInteger;
--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -3,24 +3,69 @@ Given a full sudoku board, return 'solved' if the board is solved, or 'invalid' 
 
 A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9.
 
-Input: A String representing the board. 
+Input: A String representing the board.
 Output: 'solved' if the board is valid, 'invalid' if it isn't
 
-Example input: 
-"735814296\n
-896275314\n
-214963857\n
-589427163\n
-362189745\n
-471356982\n
-923541678\n
-648792531\n
+Example input:
+"735814296
+896275314
+214963857
+589427163
+362189745
+471356982
+923541678
+648792531
 157638429"
 */
 
 
 
 function sudokuChecker(board) {
-  // Your code here.
-  
+  board = sudokuMatrix(board)
+  var rows = [[],[],[],[],[],[],[],[],[]]
+  var columns = [[],[],[],[],[],[],[],[],[]]
+  var boxes = [[],[],[],[],[],[],[],[],[]]
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+
+      let cell = board[i][j];
+
+
+        if (rows[i].includes(cell)) {
+          return false
+        } else {
+          rows[i].push(cell);
+        }
+        if (columns[j].includes(cell)) {
+          return false;
+        } else {
+        columns[j].push(cell);
+        }
+
+        let boxIndex = Math.floor((i / 3)) * 3 + Math.floor(j / 3);
+
+        if (boxes[boxIndex].includes(cell)) {
+          return false;
+        } else {
+          boxes[boxIndex].push(cell);
+        }
+
+      }
+    }
+
+
+  return true;
+}
+
+function sudokuMatrix(string) {
+  var count = 0;
+  var myArray = []
+  for (var i = 0; i < 9; i++) {
+    myArray[i] = []
+    for (var j = 0; j < 9; j++) {
+      myArray[i][j] = string[count];
+      count++;
+    }
+  }
+  return myArray;
 }
