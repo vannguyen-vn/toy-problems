a1e1a36075dcb986be073e83a5f0e8e6811ec169
Iris Huang <yilu.huang@gmail.com>
2021-10-01 19:58:27 -0700

    1st attempt ;

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -1,17 +1,23 @@
 /**
- * Write a function that takes a number as its argument and 
+ * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
- * 
+ *
  * Whole numbers and mixed fractions should be returned as irregular fractions
- * 
+ *
  * Example: toFraction(3.0) === '3/1'
- * 
+ *
  * Example: toFraction(2.5) === '5/2'
  *
  */
 
 var toFraction = function(number) {
-  // Your code here
+  if (Number.isInteger(number)) {
+    return `${number}/1`;
+  } else if (number < 1) {
+    return `1/${1/number}`;
+  } else {
+    return `${1 + Number.parseInt * (1/(number - Number.parseInt(number)))}/${1/(number - Number.parseInt(number))}`;
+  }
 };
--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,24 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  var count = 0;
+  var first = Number.parseInt(Math.log10(number));
+  var result = 0;
+  var cur = number;
+  while (first >= 0) {
+    result += Number.parseInt(cur / Math.pow(10, first)) * Math.pow(10, count);
+    cur -= Number.parseInt(cur / Math.pow(10, first)) * Math.pow(10, first);
+    count++;
+    first--;
+  }
+  return result;
 }
