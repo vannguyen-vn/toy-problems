d8c6e458b5012ff77ddf8295dce687fc0b8292af
Kevin Gao <kevinzhugao@gmail.com>
2021-10-03 21:49:03 -0700

    Completed reverseInteger function

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,25 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  var result = [];
+  var recursiveFunction = (number, subtract) => {
+    var count = 0;
+    while (number % (subtract * 10) !== 0) {
+      count++;
+      number -= subtract;
+    }
+    result.push(count);
+    if (number > 0) recursiveFunction(number, subtract * 10)
+  }
+  recursiveFunction(number, 1);
+  return result.join('');
 }
