4d78bb568ef2e61dfdd9b9195998ee5df6990dc8
Jandean Deocampo <jandeo92@gmail.com>
2021-10-04 08:47:25 -0700

    Attempt 1 at integerReverse

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -1,17 +1,28 @@
 /**
- * Write a function that takes a number as its argument and 
+ * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
- * 
+ *
  * Whole numbers and mixed fractions should be returned as irregular fractions
- * 
+ *
  * Example: toFraction(3.0) === '3/1'
- * 
+ *
  * Example: toFraction(2.5) === '5/2'
+ *
+ * Example: toFraction(3.75) === '15/4'
  *
  */
 
 var toFraction = function(number) {
-  // Your code here
+  number = Math.ceil(number) % number > 0 ? number.toString() : number + '.0';
+  const fractions = number.split('.');
+  let numerator = 0;
+
+  while(fractions[1] !== '0') {
+    const num = 1 / (1 - Number(fractions[1]));
+    fractions = num.split('.');
+  }
+  const denominator = Number(fractions[1]);
+  console.log(denominator);
 };
--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,25 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  let reverse = 0;
+  let rem = 0;
+
+  while (number > 0) {
+    reverse *= 10;
+    rem = number % 10;
+    number = (number - rem) / 10;
+
+    reverse += rem;
+  }
+
+  return reverse;
 }
