621cce1798143536d6f6e51f1b286af47dd912fb
Matthew Boyle <matthewtboyle89@gmail.com>
2021-10-04 23:28:20 -0700

    second attempt

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -1,11 +1,3 @@
-/**
-* Implement a function that finds the longest palindrome in a given string.
-* For example, in the string "My dad is a racecar athlete", the longest
-* palindrome is "a racecar a". Count whitespaces as valid characters. Other
-* palindromes in the above string include "dad", "ete", " dad " (including
-* whitespace on each side of dad).
-*/
-
 const reverseString = (string) => {
   return string.split('').reverse().join('')
 };
@@ -15,7 +7,7 @@ const isPalindrome = (string) => {
   return string === reverseString(string);
 }
 
-var longestPalindrome = function (string) {
+const longestPalindrome = function (string) {
   string = string.toLowerCase();
   var palindromeList = [];
   var possiblePalindrome = ''
@@ -23,22 +15,21 @@ var longestPalindrome = function (string) {
     var firstPointer = string[i];
     for (var j = string.length - 1; j >= 0; j--) {
       var lastPointer = string[j];
-      if (j === i) {
-        break
-      } else if (firstPointer === lastPointer) {
+     if (firstPointer === lastPointer) {
         possiblePalindrome = string.slice(i, [j+1])
         if (isPalindrome(possiblePalindrome)) {
           palindromeList.push(possiblePalindrome)
-
         }
+      } else if (j < i) {
+        break;
       }
     }
   }
-  var longestPalindrome = possiblePalindrome[0];
+  var longest = possiblePalindrome[0];
   for (var k = 0; k < palindromeList.length; k++) {
-    var palindrome = palindromeList[k];
-    if (palindrome.length > longestPalindrome.length) {
-      longestPalindrome = palindrome;
+    var palindrome = palindromeList[i];
+    if (palindrome.length > longest.length) {
+      longest = palindrome;
     }
   }
   return longestPalindrome;
