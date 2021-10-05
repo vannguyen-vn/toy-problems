da6e1510c656632b6d6137e4d8c82d99da145fb8
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-04 19:57:01 -0700

    planning for sodoku checker

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -3,10 +3,10 @@ Given a full sudoku board, return 'solved' if the board is solved, or 'invalid' 
 
 A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9.
 
-Input: A String representing the board. 
+Input: A String representing the board.
 Output: 'solved' if the board is valid, 'invalid' if it isn't
 
-Example input: 
+Example input:
 "735814296\n
 896275314\n
 214963857\n
@@ -19,8 +19,19 @@ Example input:
 */
 
 
-
+// input: string (representing a sudoku board)
+// output: string, 'solved' or 'invalid'
+// constraints: each row and column has numbers 1 - 9
+// edge cases: N/A
 function sudokuChecker(board) {
-  // Your code here.
-  
+  // create an object to organize (each key called row(1-9) or column(1-9))
+  // split the string on /n into an array of numbers
+  // loop through the array
+     // add a key to the obj called row + index
+     // loop through each row of numbers
+       // push each number to an array
+      // set the array as the value at that key
+
+  // sort each array and loop through to make sure they have 1 - 9
+  // the check that each column includes each number 1 - 9 ?
 }
