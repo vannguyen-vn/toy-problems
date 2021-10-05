284929d3fc107bb97be0073edf2e2280d01fe7c7
Jandean Deocampo <jandeo92@gmail.com>
2021-10-05 08:43:12 -0700

    Longest Palindrome

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -7,4 +7,30 @@
 */
 
 var longestPalindrome = function (string) {
+  var length = string.length;
+  var result = '';
+
+  var centeredPalindrome = function(left, right) {
+    while (left >= 0 && right < length
+           && string[left] === string[right]) {
+      left--;
+      right++;
+    }
+
+    return string.slice(left + 1, right);
+  };
+
+  for (var i = 0; i < length; i++) {
+    var oddPal = centeredPalindrome(i - 1, i + 1);
+    var evenPal = centeredPalindrome(i, i + 1);
+
+    if (oddPal.length > result.length) {
+      result = oddPal;
+    }
+    if (evenPal.length > result.length) {
+      result = evenPal;
+    }
+  }
+
+  return result;
 };
