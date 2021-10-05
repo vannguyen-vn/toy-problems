c7af39525f2cbe9ce061541cb081863eff82c32b
Matthew Boyle <matthewtboyle89@gmail.com>
2021-10-04 23:16:42 -0700

    longest palindrome attempt

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,5 +6,40 @@
 * whitespace on each side of dad).
 */
 
+const reverseString = (string) => {
+  return string.split('').reverse().join('')
+};
+
+const isPalindrome = (string) => {
+  string = string.toLowerCase()
+  return string === reverseString(string);
+}
+
 var longestPalindrome = function (string) {
+  string = string.toLowerCase();
+  var palindromeList = [];
+  var possiblePalindrome = ''
+  for (var i = 0; i < string.length; i++) {
+    var firstPointer = string[i];
+    for (var j = string.length - 1; j >= 0; j--) {
+      var lastPointer = string[j];
+      if (j === i) {
+        break
+      } else if (firstPointer === lastPointer) {
+        possiblePalindrome = string.slice(i, [j+1])
+        if (isPalindrome(possiblePalindrome)) {
+          palindromeList.push(possiblePalindrome)
+
+        }
+      }
+    }
+  }
+  var longestPalindrome = possiblePalindrome[0];
+  for (var k = 0; k < palindromeList.length; k++) {
+    var palindrome = palindromeList[k];
+    if (palindrome.length > longestPalindrome.length) {
+      longestPalindrome = palindrome;
+    }
+  }
+  return longestPalindrome;
 };
