0cebc1ed11e7a103164aedbe8477b24f81afee30
John Fa <john6fa@gmail.com>
2021-10-04 23:08:14 -0700

    longest palin work

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -7,4 +7,38 @@
 */
 
 var longestPalindrome = function (string) {
+
+  var start = 0;
+  var end = 0;
+
+  for (var i = 0; i < string.length; i++) {
+    var even = palindrome(start, end);
+    var odd = palindrome(start, end + 1);
+
+    var temp = Math.max(even[1] - even[0], odd[1] - odd[0]);
+
+    if (temp > (end - start)) {
+      if (temp === (even[1] - even[0])) {
+        start = even[0];
+        end = even[1];
+      }
+    } else {
+      start = odd[0];
+      end = odd[1];
+    }
+  }
+
+  return string.substring(start, end + 1);
 };
+
+
+var palindrome = function (string, start, end) {
+  while (start >= 0 && end <= string.length - 1 && string[start] === string[end]) {
+    start--;
+    end++;
+  }
+
+  var res = [start, end];
+
+  return res;
+}
