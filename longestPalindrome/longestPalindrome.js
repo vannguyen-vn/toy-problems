909ac21683a1a2e24a807ba9d34bd864115586c5
Kevin Gao <kevinzhugao@gmail.com>
2021-10-04 19:33:53 -0700

    Completed longestPalindrome

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,5 +6,28 @@
 * whitespace on each side of dad).
 */
 
+var isPalindrome = (string) => {
+  string = string.split('');
+  while (string.length > 1) if (string.shift() !== string.pop()) return false
+  return true;
+}
+
 var longestPalindrome = function (string) {
+  let start = 0;
+  let end = 3;
+  let longest;
+
+  while (end < string.length) {
+    while (isPalindrome(string.substring(start, end))) {
+      longest = string.substring(start, end);
+      start--;
+      end++;
+    }
+    start++;
+    end++;
+  }
+
+  return longest;
 };
+
+console.log(longestPalindrome('My dad is a racecar athlete'));
