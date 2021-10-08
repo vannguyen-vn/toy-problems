7ad5fdc1de85d977a73a8ce45d6a3650b6421c64
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-07 19:36:17 -0700

    largestPorductOfThree updated to handle negatives

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -15,15 +15,17 @@ var largestProductOfThree = function(array) {
    array.sort(function(a, b) {
     return a - b;
   })
-   return array[array.length - 1] * array[array.length - 2] * array[array.length - 3]
+   var positives = array[array.length - 1] * array[array.length - 2] * array[array.length - 3]
+   var negatives = array[0] * array[1] * array[array.length - 1]
+
+   return positives < negatives ? negatives : positives
 };
 
-largestProductOfThree([2, 1, 3, 7]) === 42
-largestProductOfThree([-2, -1, -3, -7]) === - 42
-largestProductOfThree([2, 5, 3, 7])//.should.equal(105);
-largestProductOfThree([11, 7, 5, 3, 2])//.should.equal(385);
-largestProductOfThree([2, 13, 7, 3, 5, 11])//.should.equal(1001);
-largestProductOfThree([2, 11, 13, 7, 13, 3, 11, 5])//.should.equal(1859);
+// largestProductOfThree([2, 1, 3, 7]) === 42
+// largestProductOfThree([2, 5, 3, 7])//.should.equal(105);
+// largestProductOfThree([11, 7, 5, 3, 2])//.should.equal(385);
+// largestProductOfThree([2, 13, 7, 3, 5, 11])//.should.equal(1001);
+// largestProductOfThree([2, 11, 13, 7, 13, 3, 11, 5])//.should.equal(1859);
 // largestProductOfThree([2, 3, -11, 7, 5, -13])//.should.equal(1001);
 // largestProductOfThree([-31, 41, 34, -37, -17, 29])//.should.equal(47027);
 // largestProductOfThree([-1, -2, -3, -4, -5, -6])//.should.equal(-6);
