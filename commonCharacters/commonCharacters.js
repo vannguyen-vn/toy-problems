161f720c58c88084978581cb540e399571ea1e69
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-08 19:39:50 -0700

    handles multiple strings

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -15,43 +15,49 @@
 // output: string of common letters
 // constraints: look for letters in order of appearance, skip spaces, no duplicates
 // edge cases: N/A
-var commonCharacters = function(string1, string2) {
+var commonCharacters = function (string1, string2) {
   // create obj
   var inCommon = ''
-  var letters = {}
   var unique1 = uniques(string1)
-  var unique2 = uniques(string2)
 
-  // loop through string1
-  for (var i = 0; i < string2.length; i++) {
-    // if letter doesn't exist in obj
-    if (letters[string2[i]] === undefined) {
-      // add it as a key with value false (for more than two strings could use an array)
-      letters[string2[i]] = false
+  for (var j = 1; j < arguments.length; j++) {
+    var contained = stringToObj(arguments[j])
+
+    for (var i = 0; i < unique1.length; i++) {
+      var letter = unique1[i]
+      if (contained[letter]) {
+        inCommon += letter
+      }
     }
+    unique1 = inCommon
+    inCommon = ''
   }
 
-  // loop through string2
-  for (var j = 0; j < string1.length; j++) {
-     // if letter exists as a key
-     if (letters[string1[j]] !== undefined) { // if more strings, could push true or false to the array for that letter depending on if it has the letter
-       // add it to the return str
-       inCommon += string1[j]
-     }
-  }
-   return inCommon
+  return unique1
 };
 
-var uniques =  function(string) {
+var uniques = function (string) {
   var seen = {}
   var uniqueString = ''
-  var uniqueLetters = new Set (string.split(''))
+  var uniqueLetters = new Set(string.split(''))
   uniqueString = Array.from(uniqueLetters).join('')
-  console.log(uniqueString)
   return uniqueString
 }
 
-//Example:
-console.log( commonCharacters('acexivou', 'aegihobu') ) // Returns: 'aeiou'
-commonCharacters('zyx', 'xzy')
-commonCharacters('qwerty', 'wertyu', 'ertyui', 'rtyui', 'tyuiop', 'yuiopa') // 'y'
+var stringToObj = function (string) {
+  var containsLetters = {}
+
+  for (var i = 0; i < string.length; i++) {
+    var letter = string[i]
+    containsLetters[letter] = true;
+  }
+
+  return containsLetters
+}
+
+// //Example:
+// console.log(commonCharacters('acexivou', 'aegihobu') === 'aeiou' ) // Returns: 'aeiou'
+// console.log(commonCharacters('zyx', 'xzy' ) === 'zyx' )// zyx
+// // need to implement more than two words, use arguments array?
+// console.log(commonCharacters('qwerty', 'wertyu', 'ertyui', 'rtyui', 'tyuiop', 'yuiopa' ) === 'y' )// 'y'
+// console.log(commonCharacters('zyxxxxx', 'xzy' ) === 'zyx' )
