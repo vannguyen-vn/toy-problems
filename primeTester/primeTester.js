aaf7ef2d540a70742c480326a27f57dacc5ca04c
John Fa <john6fa@gmail.com>
2021-10-20 21:45:07 -0700

    prime tester

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -4,12 +4,27 @@
  * a prime number, false if it's not.
  */
 
-var primeTester = function(n) {
+var primeTester = function (n) {
   if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
-  // TODO: return true if n is prime, false otherwise
+
+  if (n === 1) {
+    return false;
+  }
+
+  for (var i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  if (n % n === 0) {
+    return true;
+  }
+
+  return false;
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
@@ -18,7 +33,7 @@ var primeTester = function(n) {
  * saucy, check out the Sieve of Atkin.)
  */
 
-var primeSieve = function (start, end) {
-};
+// var primeSieve = function (start, end) {
+// };
 
 
