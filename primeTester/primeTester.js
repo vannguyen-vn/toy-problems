ae2a60410fb38e66ec88b1d820f97a02d0869bef
Derek Samms <derek.samms@gmail.com>
2021-10-21 19:27:47 -0700

    changed primeTester to return false for 1

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -5,7 +5,7 @@
  */
 
 var primeTester = function(n) {
-  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
+  if (typeof n !== 'number' || n <= 1 || n % 1 !== 0) {
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
