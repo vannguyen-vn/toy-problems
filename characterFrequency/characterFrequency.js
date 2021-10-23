8bc617d13cb5f1c2e7aaba1716653d20301c0b22
Derek Samms <derek.samms@gmail.com>
2021-10-22 19:29:45 -0700

    attempted characterFrequency; not complete with coinSums but want to see tests run again

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,5 +37,48 @@
 
 
 var characterFrequency = function(string) {
-  return result;
+// build result array /////
+// set result variable to empty array
+  var result = [];
+  var nested = [];
+  var sorted = [];
+// counter variable
+  var counter;
+// iterate along string
+  // for each letter
+    //if it's not in the result array
+      //push it in, set counter value to 1
+      //iterate across array from there
+        //increase counter at each occurrence
+      //push counter into result array
+    //if it is
+      //just move along
+// divide the array into nested arrays of ordered pairs
+
+  for (var i = 0; i < string.length; i++) {
+    if (!result.includes(string[i])) {
+      result.push(string[i]);
+      counter = 1;
+      for (var j = i + 1 ; j < string.length; j++) {
+        if (string[j] === string[i]) {
+          counter++;
+        }
+      }
+      result.push(counter);
+    }
+  }
+
+  for (var i = 0; i < result.length; i += 2) {
+    nested.push(result.slice(i, i + 2));
+  }
+
+// sort result array
+  nested.sort((a, b) => {
+    if (a[0] < b[0]) {
+      return -1
+    }
+  });
+  nested.sort((a, b) => (b[1] - a[1]));
+
+  return nested;
 };
