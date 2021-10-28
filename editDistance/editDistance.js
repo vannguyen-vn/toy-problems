a94be8aa38f0f79f1aefc7c2cbb70b058066317d
huongtran1993 <huongtran1993@gmail.com>
2021-10-27 17:56:04 -0700

    completed editDistance

--- editDistance/editDistance.js
+++ editDistance/editDistance.js
@@ -3,5 +3,45 @@
 // Challenge: Do this in O(m x n) time, where m, n are the respective lengths of str1 and str2.
 
 function editDistance(str1, str2) {
-  // Your code here.
-}
+  let arr1 = str1.split('');
+  let arr2 = str2.split('');
+  let isStr2Longer = arr2.length > arr1.length;
+  let lengthDiff = Math.abs(arr2.length - arr1.length);
+  let step = 0;
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === arr1[i]) {
+      continue;
+    } else if (arr2[i] !== arr1[i] && lengthDiff === 0){
+      arr1[i] = arr2[i];
+      step++;
+    } else {
+      if (isStr2Longer) {
+        let index = arr2.indexOf(arr1[i], i)
+        if (index !== -1 && index - i <= lengthDiff) {
+          for (let k = 0; k < index - i; k++) {
+            arr1.splice(i, 0, arr2[i + k]);
+          }
+          step += index - i;
+          lengthDiff -= index - i;
+          i += index - i - 1;
+        } else {
+          arr1[i] = arr2[i];
+          step++;
+        }
+      } else {
+        let index = arr1.indexOf(arr2[i], i)
+        if (index !== -1 && index - i <= lengthDiff) {
+          arr1.splice(i, index - i);
+          step += index - i;
+          lengthDiff -= index - i;
+          i--;
+        } else {
+          arr1[i] = arr2[i];
+          step++;
+        }
+      }
+    }
+  }
+  return step;
+};
